90a440da8bbd5c0c0eba8d60ec6aee538c5de887
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-09 22:11:08 -0400

    Add socket.io-client dependency and initialize socket connection in chat.tsx

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -1,4 +1,7 @@
+import { io } from 'socket.io-client'
+
 export function Chat () {
+  const socket = io('http://localhost:3000')
   return (
     <>
       <header>
@@ -7,18 +10,18 @@ export function Chat () {
         </h1>
       </header>
       <section className='border-2 border-black rounded-md overflow-hidden w-96 h-full relative'>
-        <form>
+        <form className='bottom-0 flex h-12 left-0 p-1 absolute right-0'>
           <input
             type='text'
             name='message'
             id='input'
             placeholder='Type a message'
-            className='w-full h-12 px-4'
+            className='border-2 border-black rounded-md flex m-1 pt-0 pr-2 focus:outline-none'
             autoComplete='off'
           />
           <button
             type='submit'
-            className='absolute right-0 top-0 h-12 w-12 bg-black text-white'
+            className='border-0 absolute right-0 top-0 h-12 w-12 bg-black text-white rounded-md'
           >
             Send
           </button>
