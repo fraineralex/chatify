767339684c28cf244b042053f10e434e4e2e6605
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-14 16:52:55 -0400

    Update chat item component to include hover effect, rounded border, and larger avatar size

--- client/src/components/chat/chat-item.tsx
+++ client/src/components/chat/chat-item.tsx
@@ -15,23 +15,29 @@ export function ChatItem ({
   unreadMessages
 }: Props) {
   return (
-    <li className='flex items-center space-x-2 hover:bg-gray-300 border-2 border-transparent rounded-md cursor-pointer p-1 w-full'>
+    <li className='flex items-center space-x-2 hover:bg-gray-300 border-2 border-transparent rounded-md cursor-pointer p-1 py-2 w-full'>
       <img
         src={user.avatar}
-        width='40'
-        height='40'
+        width='50'
+        height='50'
         alt={`Chat avatar of ${user.name}`}
         className='rounded-full'
-        style={{ aspectRatio: 40 / 40, objectFit: 'cover' }}
+        style={{ aspectRatio: 50 / 50, objectFit: 'cover' }}
       />
-      <article className='grid grid-cols-6'>
-        <div className='col-span-5'>
-          <h2 className='text-base font-bold'>{user.name}</h2>
-          <p className='text-sm text-gray-500'>{lastMessage}</p>
+      <article className='items-center text-left w-full'>
+        <div className='flex overflow-hidden text-left flex-grow w-full justify-between'>
+          <h2 className='text-base font-medium inline-flex overflow-hidden items-center'>
+            {user.name}
+          </h2>
+          <small className='text-xs text-gray-500 text-right inline-flex mt-1'>
+            {lastMessageDate}
+          </small>
         </div>
-        <aside className='col-span-1'>
-          <small className='text-xs text-gray-500'>{lastMessageDate}</small>
-          <span className='inline-flex items-center justify-center whitespace-nowrap text-xs font-medium border border-input bg-background h-5 w-5 px-1 py-2 rounded-full ms-8'>
+        <aside className='flex items-center text-left overflow-hidden flex-grow w-full justify-between'>
+          <p className='text-sm text-gray-500 inline-flex overflow-hidden items-center'>
+            {lastMessage}
+          </p>
+          <span className='inline-flex items-center justify-center whitespace-nowrap text-xs font-medium border border-input bg-background h-5 w-5 px-1 py-2 rounded-full'>
             {unreadMessages}
           </span>
         </aside>
