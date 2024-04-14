6b55275d5d4bbc5badfc84924913005fc4b8c1c0
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-13 23:58:53 -0400

    Update sidebar component to use array of chat objects instead of hardcoding chat items

--- client/src/components/layout/sidebar.tsx
+++ client/src/components/layout/sidebar.tsx
@@ -1,38 +1,52 @@
 import { ChatItem } from '../chat/chat-item'
 
+const chats = [
+  {
+    user: {
+      name: 'Frainer',
+      avatar: '/frainer.jpeg'
+    },
+    lastMessage: 'Last message here',
+    lastMessageDate: '10:07 AM',
+    unreadMessages: 1
+  },
+  {
+    user: {
+      name: 'Frainer',
+      avatar: '/frainer.jpeg'
+    },
+    lastMessage: 'Last message here',
+    lastMessageDate: '10:07 AM',
+    unreadMessages: 3
+  },
+  {
+    user: {
+      name: 'Frainer',
+      avatar: '/frainer.jpeg'
+    },
+    lastMessage: 'Last message here',
+    lastMessageDate: '10:07 AM',
+    unreadMessages: 4
+  },
+  {
+    user: {
+      name: 'Frainer',
+      avatar: '/frainer.jpeg'
+    },
+    lastMessage: 'Last message here',
+    lastMessageDate: '10:07 AM',
+    unreadMessages: 2
+  }
+]
+
 export function Sidebar () {
   return (
     <div className='flex flex-col border-r'>
       <nav className='p-4 space-y-4'>
         <ul className='space-y-4'>
-          <ChatItem
-            image='/frainer.jpeg'
-            name='Frainer'
-            lastMessage='Last message here'
-            lastMessageDate='10:07 AM'
-            unreadMessages={1}
-          />
-          <ChatItem
-            image='/frainer.jpeg'
-            name='Frainer'
-            lastMessage='Last message here'
-            lastMessageDate='10:07 AM'
-            unreadMessages={3}
-          />
-          <ChatItem
-            image='/frainer.jpeg'
-            name='Frainer'
-            lastMessage='Last message here'
-            lastMessageDate='10:07 AM'
-            unreadMessages={4}
-          />
-          <ChatItem
-            image='/frainer.jpeg'
-            name='Frainer'
-            lastMessage='Last message here'
-            lastMessageDate='10:07 AM'
-            unreadMessages={2}
-          />
+          {chats.map((chat, index) => (
+            <ChatItem key={index} {...chat} />
+          ))}
         </ul>
       </nav>
     </div>
