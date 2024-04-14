f369c31989ff058a069b18f4b19e08a01838cf9e
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-14 01:14:51 -0400

    Refactor chat form component to include socket.io integration and message sending functionality

--- client/src/components/chat/form.tsx
+++ client/src/components/chat/form.tsx
@@ -1,8 +1,28 @@
 import { AttachFile, Emoji, Send } from '../general/svg-icons'
 
-export function Form () {
+import { Socket } from 'socket.io-client'
+
+interface Props {
+  socket: Socket
+}
+
+export function Form ({ socket }: Props) {
+  const sendMessage = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+    const message = formData.get('message') as string
+    if (!message) return
+    socket.emit('chat message', message)
+    console.log('Message sent:', message)
+    form.reset()
+  }
+
   return (
-    <form className='flex items-center p-2 border-t w-full'>
+    <form
+      className='flex items-center p-2 border-t w-full'
+      onSubmit={sendMessage}
+    >
       <button className='inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground p-4 rounded-full'>
         <Emoji className='w-4 h-4' />
         <span className='sr-only'>Insert emoji</span>
@@ -14,8 +34,12 @@ export function Form () {
       <input
         className='flex h-10 w-full border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-full border-0 flex-1'
         placeholder='Type a message'
+        name='message'
       />
-      <button className='ms-2 inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground rounded-full w-6 h-6'>
+      <button
+        type='submit'
+        className='ms-2 inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground rounded-full w-6 h-6'
+      >
         <Send className='w-5 h-5' />
         <span className='sr-only'>Send</span>
       </button>
