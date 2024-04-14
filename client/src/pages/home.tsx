a4fa1000167faa0d48a06197e90ce4b1d208958c
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-13 23:47:15 -0400

    Add home page component to client/src/pages/home.tsx

---
+++ client/src/pages/home.tsx
@@ -0,0 +1,14 @@
+//import { io } from 'socket.io-client'
+
+import { Chat } from '../components/chat'
+import { Sidebar } from '../components/layout/sidebar'
+
+export function Home () {
+  //const socket = io('http://localhost:3000')
+  return (
+    <section className='grid grid-cols-4 h-screen border rounded-lg overflow-hidden font-sans'>
+      <Sidebar />
+      <Chat />
+    </section>
+  )
+}
