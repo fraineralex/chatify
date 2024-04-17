e67fc59805c387d53e86f4dfa3deddd4c8fb2706
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-16 21:44:12 -0400

    Update useChatMessage hook to use new socket event constants and message types

--- client/src/hooks/useChatMessage.ts
+++ client/src/hooks/useChatMessage.ts
@@ -1,12 +1,13 @@
-import { Message, ServerMessage } from './../types/chat.d'
+import { Message, ServerMessageDB } from './../types/chat.d'
 import { useEffect } from 'react'
 import { useSocketStore } from '../store/socket'
+import { SOCKET_EVENTS } from '../constants'
 
 export const useChatMessage = () => {
   const { setMessage, socket, setServerOffset } = useSocketStore()
 
   useEffect(() => {
-    socket?.on('chat message', (message: ServerMessage) => {
+    socket?.on(SOCKET_EVENTS.CHAT_MESSAGE, (message: ServerMessageDB) => {
       const newMessages: Message = {
         uuid: message.uuid,
         content: message.content,
@@ -26,7 +27,7 @@ export const useChatMessage = () => {
     })
 
     return () => {
-      socket?.off('chat message')
+      socket?.off(SOCKET_EVENTS.CHAT_MESSAGE)
     }
   }, [])
 }
