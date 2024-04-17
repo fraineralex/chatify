f5c28e283b7f57d246df02db8890913f99590076
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-16 21:44:53 -0400

    Add chat types and interfaces to the server

---
+++ server/src/types/chat.d.ts
@@ -0,0 +1,18 @@
+import { MESSAGES_TYPES } from '../constants'
+
+export interface ServerMessage {
+  content: string
+  sender_id: string
+  receiver_id: string
+  type: typeof MESSAGES_TYPES[keyof typeof MESSAGES_TYPES]
+  resource_url: string | null
+  is_read: boolean
+  is_edited: boolean
+  is_deleted: boolean
+  reply_to_id: string | null
+}
+
+export interface ServerMessageDB extends ServerMessage {
+  uuid: string
+  created_at: string
+}
