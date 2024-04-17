158093e6ea9ea99d96a3ff661bc15f54ce3bf12c
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-16 21:45:12 -0400

    Add constants for socket events and message types to the server

---
+++ server/src/constants/index.ts
@@ -0,0 +1,19 @@
+export const SOCKET_EVENTS = {
+  CONNECTION: 'connection',
+  DISCONNECT: 'disconnect',
+  CHAT_MESSAGE: 'chat_message',
+  NEW_MESSAGE: 'new_message',
+  EDIT_MESSAGE: 'edit_message',
+  DELETE_MESSAGE: 'delete_message',
+  READ_MESSAGE: 'read_message'
+} as const
+
+export const MESSAGES_TYPES = {
+  TEXT: 'text',
+  IMAGE: 'image',
+  VIDEO: 'video',
+  AUDIO: 'audio',
+  FILE: 'file',
+  EMOJI: 'emoji',
+  STICKER: 'sticker'
+} as const
