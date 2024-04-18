adc0809247d426f94d90d696973870ad74ecf285
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-17 23:26:57 -0400

    Refactor chat component to update current chat messages and scroll to the end

--- client/src/components/chat/index.tsx
+++ client/src/components/chat/index.tsx
@@ -3,31 +3,34 @@ import { Header } from './header'
 import { Message } from './message'
 import './chat.css'
 import { useSocketStore } from '../../store/socket'
+import { useRef } from 'react'
 
 export function Chat () {
   const { messages, currentChat } = useSocketStore()
   const currentChatMessages = messages
     .filter(
       message =>
-        message.sender_id.username === currentChat ||
-        message.receiver_id.username === currentChat
+        message.senderId.username === currentChat.name ||
+        message.receiverId.username === currentChat.name
     )
     .sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime())
 
+  const messagesEndRef = useRef<HTMLDivElement>(null)
+  if (messagesEndRef.current) {
+    messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
     <main className='flex flex-col h-screen p-4 pr-1 pt-0 border-b col-span-3'>
-      {currentChat !== undefined ? (
+      {currentChat.name ? (
         <>
-          <Header name={currentChat} image={`/${currentChat}.webp`} />
-          <div className='flex-1 p-4 space-y-4 overflow-y-auto my-5'>
+          <Header name={currentChat.name} image={`/${currentChat.name}.webp`} />
+          <div
+            className='flex-1 p-4 space-y-4 overflow-y-auto my-5 scroll-smooth'
+            ref={messagesEndRef}
+          >
             {currentChatMessages.map((message, index) => (
-              <Message
-                key={message.uuid ?? index}
-                content={message.content}
-                createdAt={message.createdAt}
-                sender_id={message.sender_id}
-                receiver_id={message.receiver_id}
-              />
+              <Message key={message.uuid ?? index} {...message} />
             ))}
           </div>
           <Form />
