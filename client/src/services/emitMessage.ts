05fa5b5f62cc9139347e9f17dd6945ecd0c3200d
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-17 23:27:25 -0400

    Remove unused emitMessage function from client/src/services/emitMessage.ts

--- client/src/services/emitMessage.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import { useSocketStore } from '../store/socket'
-
-export function emitMessage (content: string) {
-  const socket = useSocketStore(state => state.socket)
-
-  socket.emit('chat message', content)
-}
