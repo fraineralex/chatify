603a0dc7ca04f15715e217a2a4e8fdd65cf0cc49
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-17 23:27:18 -0400

    Refactor useChatMessage hook to handle chat message events and read messages

--- client/src/hooks/useChatMessage.ts
+++ client/src/hooks/useChatMessage.ts
@@ -4,30 +4,47 @@ import { useSocketStore } from '../store/socket'
 import { SOCKET_EVENTS } from '../constants'
 
 export const useChatMessage = () => {
-  const { setMessage, socket, setServerOffset } = useSocketStore()
+  const { setMessage, socket, setServerOffset, currentChat, messages, replaceMessage } =
+    useSocketStore()
 
   useEffect(() => {
-    socket?.on(SOCKET_EVENTS.CHAT_MESSAGE, (message: ServerMessageDB) => {
+    socket.on(SOCKET_EVENTS.CHAT_MESSAGE, (message: ServerMessageDB) => {
       const newMessages: Message = {
         uuid: message.uuid,
         content: message.content,
         createdAt: new Date(message.created_at),
-        sender_id: {
+        senderId: {
           username: message.sender_id,
           avatar: `/${message.sender_id}.webp`
         },
-        receiver_id: {
+        receiverId: {
           username: message.receiver_id,
           avatar: `/${message.receiver_id}.webp`
-        }
+        },
+        type: message.type,
+        isDeleted: !!message.is_deleted,
+        isEdited: !!message.is_edited,
+        isRead: currentChat.name === message.receiver_id || !!message.is_read,
+        replyToId: message.reply_to_id,
+        resourceUrl: message.resource_url
       }
 
       setMessage(newMessages)
       setServerOffset(message.created_at)
     })
 
+    socket.on(SOCKET_EVENTS.READ_MESSAGE, (message: ServerMessageDB) => {
+      const newMessage = messages.find(m => m.uuid === message.uuid)
+      if (!newMessage) return
+      replaceMessage({
+        ...newMessage,
+        isRead: true
+      })
+    })
+
     return () => {
-      socket?.off(SOCKET_EVENTS.CHAT_MESSAGE)
+      socket.off(SOCKET_EVENTS.CHAT_MESSAGE)
+      socket.off(SOCKET_EVENTS.READ_MESSAGE)
     }
   }, [])
 }
