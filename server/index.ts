ccfeb51da943eedd9eb90e2ede00176dee83fc18
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-17 23:29:19 -0400

    Add feature to update as read the messages in the database

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,17 @@
 import express from 'express'
 import logger from 'morgan'
-import path from 'path'
+//import path from 'path'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { createClient } from '@libsql/client'
-import { SOCKET_EVENTS } from './src/constants'
-import { ServerMessage, ServerMessageDB } from './src/types/chat'
+import {
+  MessagesToRead,
+  ServerMessage,
+  ServerMessageDB
+} from './src/types/chat.js'
+import { SOCKET_EVENTS } from './src/constants/index.js'
 
 dotenv.config({ path: '.env.local' })
 const port = process.env.PORT ?? 3000
@@ -49,8 +53,7 @@ io.on(SOCKET_EVENTS.CONNECTION, async socket => {
     console.log('An user has disconnected!')
   })
 
-  socket.on(SOCKET_EVENTS.CHAT_MESSAGE, async (message: ServerMessage) => {
-    let result
+  socket.on(SOCKET_EVENTS.NEW_MESSAGE, async (message: ServerMessage) => {
     const uuid = crypto.randomUUID()
     const created_at = new Date().toISOString()
     const createdMessage: ServerMessageDB = {
@@ -60,8 +63,8 @@ io.on(SOCKET_EVENTS.CONNECTION, async socket => {
     }
 
     try {
-      result = await db.execute({
-        sql: 'INSERT INTO messages (uuid, content, sender_id, receiver_id, is_read, is_edited, is_deleted, is_deletedreply_to_id, type, resource_url, created_at) VALUES (:uuid, :content, :sender_id, :receiver_id, :is_read, :is_edited, :is_deleted, :reply_to_id, :type, :resource_url, :created_at)',
+      await db.execute({
+        sql: 'INSERT INTO messages (uuid, content, sender_id, receiver_id, is_read, is_edited, is_deleted, reply_to_id, type, resource_url, created_at) VALUES (:uuid, :content, :sender_id, :receiver_id, :is_read, :is_edited, :is_deleted, :reply_to_id, :type, :resource_url, :created_at)',
         args: {
           uuid,
           ...message,
@@ -73,28 +76,67 @@ io.on(SOCKET_EVENTS.CONNECTION, async socket => {
       return
     }
 
-    io.emit(SOCKET_EVENTS.NEW_MESSAGE, createdMessage)
+    io.emit(SOCKET_EVENTS.CHAT_MESSAGE, createdMessage)
   })
+
+  socket.on(
+    SOCKET_EVENTS.READ_MESSAGE,
+    async (messagesToRead: MessagesToRead) => {
+      console.log(messagesToRead)
+      let selectStatementResult
+      let updateStatementResult
+      try {
+        selectStatementResult = await db.execute({
+          sql: 'SELECT * FROM messages WHERE sender_id = :sender_id AND receiver_id = :receiver_id AND is_read = FALSE',
+          args: { ...messagesToRead }
+        })
+
+        updateStatementResult = await db.execute({
+          sql: 'UPDATE messages SET is_read = TRUE WHERE sender_id = :sender_id AND receiver_id = :receiver_id AND is_read = FALSE',
+          args: { ...messagesToRead }
+        })
+
+        console.log(
+          'OK: ',
+          selectStatementResult.rows.length ===
+            updateStatementResult.rowsAffected
+        )
+
+        if (
+          selectStatementResult.rowsAffected ===
+            updateStatementResult.rowsAffected &&
+          selectStatementResult.rows.length > 0
+        ) {
+          selectStatementResult.rows.forEach(row => {
+            const message = {
+              ...row
+            }
+            socket.emit(SOCKET_EVENTS.READ_MESSAGE, message)
+          })
+        }
+      } catch (error) {
+        console.error(error)
+        return
+      }
+    }
+  )
 
   if (!socket.recovered) {
     try {
       const results = await db.execute({
-        sql: `SELECT uuid, content, sender_id, receiver_id, created_at FROM messages WHERE created_at > ?`,
+        sql: `SELECT uuid, content, sender_id, receiver_id, is_read, is_edited, is_deleted, reply_to_id, type, resource_url, created_at FROM messages WHERE created_at > ?`,
         args: [socket.handshake.auth.serverOffset ?? 0]
       })
 
       results.rows.forEach(row => {
         const message = {
-          uuid: row.uuid,
-          content: row.content,
-          created_at: row.created_at,
-          sender_id: row.sender_id,
-          receiver_id: row.receiver_id
+          ...row
         }
         socket.emit(SOCKET_EVENTS.CHAT_MESSAGE, message)
       })
     } catch (e) {
       console.error(e)
+      return
     }
   }
 })
