4c02b394f33c1ec79d6b7cb7256e12afb3c55849
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-19 18:14:47 -0400

    Update main.tsx to use Auth0Provider for user authentication

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
+import { Auth0Provider } from '@auth0/auth0-react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Auth0Provider
+    domain='chatify-app.us.auth0.com'
+    clientId='Z3GOlIbkpW5YIpZNSjyKXaHcDrH490g2'
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: `https://chatify-app.us.auth0.com/api/v2/`,
+      scope: 'read:current_user update:current_user_metadata'
+    }}
+  >
     <App />
-  </StrictMode>
+  </Auth0Provider>
 )
