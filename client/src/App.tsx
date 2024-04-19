ed6d71528d9e95eb35ca34bdd34bd559e14eec35
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-19 18:14:54 -0400

    Update App.tsx to integrate user authentication with Auth0

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
+import Home from './components/home'
 import { ChatScreen } from './pages/chat'
+import { useAuth0 } from '@auth0/auth0-react'
+import { useSocketStore } from './store/socket'
 
 function App () {
-  return (
-    <>
-      <ChatScreen />
-    </>
-  )
+  const { isAuthenticated, isLoading, user } = useAuth0()
+  const { socket, setSocket } = useSocketStore()
+
+  if (isLoading) return <div>Loading...</div>
+
+  if (isAuthenticated && user) {
+    const newSocket = socket
+    newSocket.auth = {
+      ...newSocket.auth,
+      user: user
+    }
+
+    setSocket(newSocket)
+  }
+
+  return <>{isAuthenticated ? <ChatScreen /> : <Home />}</>
 }
 
 export default App
