7f7aa708d15351218bd65453aeba04b23ac77483
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-21 00:46:03 -0400

    Update chat.d.ts to include chat_id in ServerMessage and MessagesToRead interfaces

--- server/src/types/chat.d.ts
+++ server/src/types/chat.d.ts
@@ -1,9 +1,12 @@
 import { MESSAGES_TYPES } from '../constants/index.ts'
 
+type uuid = `${string}-${string}-${string}-${string}-${string}`
+
 export interface ServerMessage {
   content: string
   sender_id: string
   receiver_id: string
+  chat_id: uuid
   type: typeof MESSAGES_TYPES[keyof typeof MESSAGES_TYPES]
   resource_url: string | null
   is_read: boolean
@@ -13,11 +16,12 @@ export interface ServerMessage {
 }
 
 export interface ServerMessageDB extends ServerMessage {
-  uuid: string
+  uuid: uuid
   created_at: string
 }
 
 export interface MessagesToRead {
+  chat_id: uuid
   sender_id: string
   receiver_id: string
 }
