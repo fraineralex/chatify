b6fe519eec7d2df754ca1d94c1cf5377cb1c24a8
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-21 13:27:48 -0400

    Add ChatRouter to handle chat-related API requests

---
+++ server/src/router/chat.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+import { ChatController } from '../controllers/chat.js'
+import { Client } from '@libsql/client'
+
+export class ChatRouter {
+  private chatController: ChatController
+
+  constructor (client: Client) {
+    this.chatController = new ChatController(client)
+  }
+
+  init () {
+    const router = Router()
+
+    router.post('/', this.chatController.createChat.bind(this.chatController))
+
+    return router
+  }
+}
