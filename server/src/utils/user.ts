bbd75ef1c85397efd1b4016d092cf166340dcfa4
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-21 00:45:57 -0400

    Add getUserById function to fetch user data from Auth0 API

---
+++ server/src/utils/user.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const DOMAIN = process.env.AUTH0_DOMAIN ?? ''
+const API_ACCESS_TOKEN = process.env.AUTH0_MGMT_API_TOKEN ?? ''
+
+export async function getUserById (id: string) {
+  const config = {
+    method: 'GET',
+    url: `https://${DOMAIN}/api/v2/users/${id}`,
+    headers: {
+      authorization: `Bearer ${API_ACCESS_TOKEN}`
+    }
+  }
+
+  const response = await axios.request(config)
+  return response.data
+}
