59708e51da04a5364831239abd3971ffd8df7d64
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-22 00:17:11 -0400

    Refactor useChatMessage hook to include chatId in received messages

--- client/src/hooks/useChatMessage.ts
+++ client/src/hooks/useChatMessage.ts
@@ -1,17 +1,23 @@
-import { Message, ServerMessageDB } from './../types/chat.d'
+/* eslint-disable no-extra-semi */
+import { Chat, Message, ServerMessageDB } from './../types/chat.d'
 import { useEffect } from 'react'
 import { useSocketStore } from '../store/socket'
 import { SOCKET_EVENTS } from '../constants'
+import { useAuth0 } from '@auth0/auth0-react'
+
+const API_URL = import.meta.env.VITE_SERVER_DOMAIN ?? 'http://localhost:3000'
 
 export const useChatMessage = () => {
   const {
     setMessage,
     socket,
     setServerOffset,
-    currentChat,
     messages,
-    replaceMessage
+    replaceMessage,
+    setChat
   } = useSocketStore()
+
+  const { user: loggedUser } = useAuth0()
 
   useEffect(() => {
     socket.on(SOCKET_EVENTS.CHAT_MESSAGE, (message: ServerMessageDB) => {
@@ -21,10 +27,11 @@ export const useChatMessage = () => {
         createdAt: new Date(message.created_at),
         senderId: message.sender_id,
         receiverId: message.receiver_id,
+        chatId: message.chat_id,
         type: message.type,
         isDeleted: !!message.is_deleted,
         isEdited: !!message.is_edited,
-        isRead: currentChat.name === message.receiver_id || !!message.is_read,
+        isRead: !!message.is_read,
         replyToId: message.reply_to_id,
         resourceUrl: message.resource_url
       }
@@ -46,5 +53,18 @@ export const useChatMessage = () => {
       socket.off(SOCKET_EVENTS.CHAT_MESSAGE)
       socket.off(SOCKET_EVENTS.READ_MESSAGE)
     }
-  }, [])
+  }, [socket])
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await fetch(
+        `${API_URL}/chats?user_id=${loggedUser?.sub}`
+      )
+      const chats: Chat[] = await response.json()
+
+      chats.forEach(chat => {
+        setChat(chat)
+      })
+    })()
+  }, [socket])
 }
