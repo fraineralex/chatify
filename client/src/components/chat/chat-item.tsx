c78f182cc3d11ba409e61de1867eb1d5e5f09279
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-22 00:01:40 -0400

    Refactor ChatItem component to use uuid instead of user name for identifying chats

--- client/src/components/chat/chat-item.tsx
+++ client/src/components/chat/chat-item.tsx
@@ -1,35 +1,32 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import { SOCKET_EVENTS } from '../../constants'
 import { useSocketStore } from '../../store/socket'
-import { MessagesToRead } from '../../types/chat'
-
-interface Props {
-  user: {
-    name: string
-    avatar: string
-  }
-  lastMessage: string
-  lastMessageDate: string
-  unreadMessages: number
-}
+import { Chat, MessagesToRead } from '../../types/chat'
 
 export function ChatItem ({
+  uuid,
   user,
   lastMessage,
-  lastMessageDate,
-  unreadMessages
-}: Props) {
-  const { currentChat, setCurrentChatName, socket, loggedUser } =
-    useSocketStore()
-  const isCurrentChat = currentChat.name === user.name
+  unreadMessages,
+  createdAt
+}: Chat) {
+  const { currentChat, setCurrentChat, socket } = useSocketStore()
+  const { user: loggedUser } = useAuth0()
+  const isCurrentChat = currentChat?.uuid === uuid
+  const lastMessageDate = lastMessage?.createdAt || createdAt
 
   const openChat = () => {
     if (isCurrentChat) return
 
-    localStorage.setItem(currentChat.name, currentChat.draft || '')
-    setCurrentChatName(user.name)
+    if (currentChat) {
+      localStorage.setItem(currentChat.uuid, currentChat.draft || '')
+    }
+
+    setCurrentChat(uuid)
     const messagesToRead: MessagesToRead = {
+      chat_id: uuid,
       sender_id: user.name,
-      receiver_id: loggedUser
+      receiver_id: loggedUser?.sub
     }
     socket.emit(SOCKET_EVENTS.READ_MESSAGE, messagesToRead)
   }
@@ -44,7 +41,7 @@ export function ChatItem ({
       onClick={openChat}
     >
       <img
-        src={user.avatar}
+        src={user.picture}
         width='50'
         height='50'
         alt={`Chat avatar of ${user.name}`}
@@ -57,12 +54,15 @@ export function ChatItem ({
             {user.name}
           </h2>
           <small className='text-xs text-gray-500 text-right inline-flex mt-1'>
-            {lastMessageDate}
+            {lastMessageDate.toLocaleString([], {
+              hour: '2-digit',
+              minute: '2-digit'
+            })}
           </small>
         </div>
         <aside className='flex items-center text-left overflow-hidden flex-grow w-full justify-between'>
           <p className='text-sm text-gray-500 inline-flex overflow-hidden items-center'>
-            {lastMessage}
+            {lastMessage?.content || 'No messages yet'}
           </p>
           {unreadMessages > 0 && (
             <span className='inline-flex items-center justify-center whitespace-nowrap text-xs font-medium border border-input bg-background h-5 w-5 px-1 py-2 rounded-full'>
