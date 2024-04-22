259d988c17f9ee64c91024d77c5d747193beae12
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-22 00:01:31 -0400

    Update Auth0 configuration in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN ?? ''
+const AUTH0_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID ?? ''
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Auth0Provider
-    domain='chatify-app.us.auth0.com'
-    clientId='Z3GOlIbkpW5YIpZNSjyKXaHcDrH490g2'
+    domain={AUTH0_DOMAIN}
+    clientId={AUTH0_CLIENT_ID}
     authorizationParams={{
-      redirect_uri: window.location.origin,
-      audience: `https://chatify-app.us.auth0.com/api/v2/`,
-      scope: 'read:current_user update:current_user_metadata'
+      redirect_uri: window.location.origin
+      /* audience: `https://${AUTH0_DOMAIN}/api/v2/`,
+      scope: 'read:current_user update:current_user_metadata' */
     }}
   >
     <App />
