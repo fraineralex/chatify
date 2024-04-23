23fb515967af66cd2a0840ba4eafc02339c10ba3
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-23 19:03:32 -0400

    Refactor Profile component to display formatted user name in profile.tsx

--- client/src/components/auth/profile.tsx
+++ client/src/components/auth/profile.tsx
@@ -3,6 +3,8 @@ import { LogOut } from 'lucide-react'
 
 export function Profile () {
   const { user, logout } = useAuth0()
+  const formatedName = user?.name?.split(' ').slice(0, 2).join(' ')
+
   return (
     <footer
       title='Log out'
@@ -14,12 +16,15 @@ export function Profile () {
     >
       <figure className='flex justify-center items-center space-x-2 group-hover:space-x-3'>
         <img
+          width='50'
+          height='50'
           src={user?.picture}
-          alt={`avatar of the user ${user?.name}`}
+          alt={`avatar of the user ${formatedName}`}
           className='w-10 h-10 rounded-full group-hover:scale-105 ease-in-out duration-300'
+          style={{ aspectRatio: 50 / 50, objectFit: 'cover' }}
         />
         <figcaption className='text-gray-700 font-semibold text-sm group-hover:text-gray-900 group-hover:scale-105 ease-in-out duration-300'>
-          {user?.name}
+          {formatedName}
         </figcaption>
       </figure>
       <LogOut className='hidden group-hover:block ease-in-out duration-300 font-semibold text-sm text-gray-900' />
