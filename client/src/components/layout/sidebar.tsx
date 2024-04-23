cdc4c458cd55229ef66a5911d03a5af23c9a2779
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-23 19:03:53 -0400

    Refactor Sidebar component to sort chats by last message date

--- client/src/components/layout/sidebar.tsx
+++ client/src/components/layout/sidebar.tsx
@@ -5,55 +5,17 @@ import { ChatItem } from '../chat/chat-item'
 export function Sidebar () {
   const { chats } = useSocketStore()
 
-  /*   messages
-    .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-    .forEach(message => {
-      const lastMessage = {
-        lastMessage: message.content,
-        lastMessageDate: message.createdAt.toLocaleString([], {
-          hour: '2-digit',
-          minute: '2-digit'
-        })
-      }
-
-      if (!chats.find(chat => chat.user.name === message.senderId.username)) {
-        chats.push({
-          user: {
-            name: message.senderId.username,
-            avatar: message.senderId.avatar
-          },
-          unreadMessages: messages.filter(
-            msg =>
-              msg.isRead === false &&
-              msg.senderId.username === message.senderId.username &&
-              msg.receiverId.username === loggedUser
-          ).length,
-          ...lastMessage
-        })
-      }
-
-      if (!chats.find(chat => chat.user.name === message.receiverId.username)) {
-        chats.push({
-          user: {
-            name: message.receiverId.username,
-            avatar: message.receiverId.avatar
-          },
-          unreadMessages: messages.filter(
-            msg =>
-              msg.isRead === false &&
-              msg.senderId.username === message.receiverId.username &&
-              msg.receiverId.username === loggedUser
-          ).length,
-          ...lastMessage
-        })
-      }
-    }) */
+  const sortedChats = chats.sort(
+    (a, b) =>
+      (b.lastMessage?.createdAt.getTime() || b.createdAt.getTime()) -
+      (a.lastMessage?.createdAt.getTime() || a.createdAt.getTime())
+  )
 
   return (
     <div className='border-r h-screen'>
       <nav className='py-4 px-1 space-y-4 h-[90%]'>
         <ul className='space-y-1'>
-          {chats.map((chat, index) => (
+          {sortedChats.map((chat, index) => (
             <ChatItem key={index} {...chat} />
           ))}
         </ul>
