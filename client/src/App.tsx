13ecd1319827d78274de219c56445ba39e8b1286
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-23 19:03:27 -0400

    Refactor App component to simplify socket authentication logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,13 @@
 import Home from './components/home'
 import { ChatScreen } from './pages/chat'
 import { useAuth0 } from '@auth0/auth0-react'
-import { useSocketStore } from './store/socket'
 
 function App () {
   const { isAuthenticated, isLoading, user } = useAuth0()
-  const { socket, setSocket } = useSocketStore()
 
   if (isLoading) return <div>Loading...</div>
 
-  if (isAuthenticated && user) {
-    const newSocket = socket
-    newSocket.auth = {
-      ...newSocket.auth,
-      user: user
-    }
-
-    setSocket(newSocket)
-  }
-
-  return <>{isAuthenticated ? <ChatScreen /> : <Home />}</>
+  return <>{isAuthenticated && user ? <ChatScreen /> : <Home />}</>
 }
 
 export default App
