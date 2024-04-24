7742dbb43a87e881d54e20abd2e305e0e9af24b8
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-24 18:24:50 -0400

    Add authentication to Home component in client/src/components/home/index.tsx

--- client/src/components/home/index.tsx
+++ client/src/components/home/index.tsx
@@ -1,6 +1,10 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import LoginButton from '../auth/login-button'
 
 export default function Home () {
+  const { isAuthenticated, user } = useAuth0()
+
+  console.log('isAuthenticated', isAuthenticated, 'user', user)
   return (
     <section className='flex flex-col h-screen place-items-center justify-center'>
       <h1 className='text-center text-2xl font-bold'>Welcome to Chatify!</h1>
