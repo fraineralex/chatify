6bf42b4f6e85b7ca6b80a8c701f77b5b26555ebf
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-24 18:24:30 -0400

    Refactor Chat component to use useChatStore instead of useSocketStore in chat/index.tsx

--- client/src/components/chat/index.tsx
+++ client/src/components/chat/index.tsx
@@ -4,14 +4,11 @@ import { Message } from './message'
 import './chat.css'
 import { useSocketStore } from '../../store/socket'
 import { useRef } from 'react'
+import { useChatStore } from '../../store/currenChat'
 
 export function Chat () {
-  const { messages, currentChat, chats } = useSocketStore()
-  const chat = chats.find(chat => chat.uuid === currentChat?.uuid)
-  const currentChatMessages = messages
-    .filter(message => message.chatId === currentChat?.uuid)
-    .sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime())
-
+  const { messages } = useSocketStore()
+  const currentChat = useChatStore(state => state.currentChat)
   const messagesEndRef = useRef<HTMLDivElement>(null)
 
   if (messagesEndRef.current) {
@@ -20,16 +17,22 @@ export function Chat () {
 
   return (
     <main className='flex flex-col h-screen p-4 pr-1 pt-0 border-b col-span-3'>
-      {chat?.user.name ? (
+      {currentChat ? (
         <>
-          <Header name={chat?.user.name} picture={`${chat?.user.picture}`} />
+          <Header
+            name={currentChat.user.name}
+            picture={`${currentChat.user.picture}`}
+          />
           <div
             className='flex-1 p-4 space-y-4 overflow-y-auto my-5 scroll-smooth'
             ref={messagesEndRef}
           >
-            {currentChatMessages.map((message, index) => (
-              <Message key={message.uuid ?? index} {...message} />
-            ))}
+            {messages
+              .filter(message => message.chatId === currentChat?.uuid)
+              .sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime())
+              .map((message, index) => (
+                <Message key={message.uuid ?? index} {...message} />
+              ))}
           </div>
           <Form />
         </>
