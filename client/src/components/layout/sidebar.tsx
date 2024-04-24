ef504f54bafb20596535aa31b7c7dcf09a5bad92
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-24 18:25:04 -0400

    Refactor Sidebar component to include Header and remove Profile in sidebar.tsx

--- client/src/components/layout/sidebar.tsx
+++ client/src/components/layout/sidebar.tsx
@@ -1,26 +1,26 @@
 import { useSocketStore } from '../../store/socket'
-import { Profile } from '../auth/profile'
 import { ChatItem } from '../chat/chat-item'
+import { Header } from './header'
 
 export function Sidebar () {
   const { chats } = useSocketStore()
 
-  const sortedChats = chats.sort(
-    (a, b) =>
-      (b.lastMessage?.createdAt.getTime() || b.createdAt.getTime()) -
-      (a.lastMessage?.createdAt.getTime() || a.createdAt.getTime())
-  )
-
   return (
     <div className='border-r h-screen'>
+      <Header />
       <nav className='py-4 px-1 space-y-4 h-[90%]'>
         <ul className='space-y-1'>
-          {sortedChats.map((chat, index) => (
-            <ChatItem key={index} {...chat} />
-          ))}
+          {chats
+            .sort(
+              (a, b) =>
+                (b.lastMessage?.createdAt.getTime() || b.createdAt.getTime()) -
+                (a.lastMessage?.createdAt.getTime() || a.createdAt.getTime())
+            )
+            .map((chat, index) => (
+              <ChatItem key={index} {...chat} />
+            ))}
         </ul>
       </nav>
-      <Profile />
     </div>
   )
 }
