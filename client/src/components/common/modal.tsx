19a5d0ae255312d62df313f8fdfee02baed866eb
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-24 18:24:35 -0400

    Add Modal component to handle pop-up modals in client/src/components/common/modal.tsx

---
+++ client/src/components/common/modal.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react'
+import { createPortal } from 'react-dom'
+
+interface ModalProps {
+  isOpen: boolean
+  onClose: () => void
+  children: ReactNode
+}
+
+export default function Modal ({ isOpen, onClose, children }: ModalProps) {
+  if (!isOpen) {
+    return null
+  }
+
+  return createPortal(
+    <>
+      <div className='fixed inset-0 z-50' onClick={onClose} />
+      {children}
+    </>,
+    document.body
+  )
+}
