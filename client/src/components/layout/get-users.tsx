6fb5945c80917aac49028217d77a73d44b280a49
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-25 01:50:12 -0400

    Refactor GetUsersModal component to use ChatItem component for rendering users

--- client/src/components/layout/get-users.tsx
+++ client/src/components/layout/get-users.tsx
@@ -1,16 +1,17 @@
 import { EllipsisVertical, SquarePen } from 'lucide-react'
 import Modal from '../common/modal'
 import { useEffect, useState } from 'react'
-import { useChatStore } from '../../store/currenChat'
 import { User } from '../../types/chat'
+import { ChatItem } from '../chat/chat-item'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const SERVER_DOMAIN =
   import.meta.env.VITE_SERVER_DOMAIN ?? 'http://localhost:3000'
 
 export function GetUsersModal () {
-  const currentChat = useChatStore(state => state.currentChat)
   const [open, setOpen] = useState(false)
   const [users, setUsers] = useState<User[]>([])
+  const loggedUser = useAuth0().user
 
   useEffect(() => {
     if (!open) return
@@ -44,33 +45,11 @@ export function GetUsersModal () {
           </div>
           <nav className='self-start w-full'>
             <ul className='space-y-1'>
-              {users.map((user, index) => (
-                <li
-                  key={index}
-                  className={`flex items-center space-x-2  border border-transparent border-b-gray-300 cursor-pointer px-2 py-2 w-full ${
-                    currentChat?.user.id === user.id
-                      ? 'bg-gray-300 rounded-md'
-                      : 'rounded-sm hover:bg-gray-200 hover:rounded-md'
-                  }`}
-                  onClick={() => {}}
-                >
-                  <img
-                    src={user.picture}
-                    width='50'
-                    height='50'
-                    alt={`Chat avatar of ${user.name}`}
-                    className='rounded-full'
-                    style={{ aspectRatio: 50 / 50, objectFit: 'cover' }}
-                  />
-                  <article className='items-center text-left w-full'>
-                    <div className='flex overflow-hidden text-left flex-grow w-full justify-between'>
-                      <h2 className='text-base font-medium inline-flex overflow-hidden items-center'>
-                        {user.name}
-                      </h2>
-                    </div>
-                  </article>
-                </li>
-              ))}
+              {users
+                .filter(user => user.id !== loggedUser?.sub)
+                .map((user, index) => (
+                  <ChatItem key={index} user={user} isNewChat />
+                ))}
             </ul>
           </nav>
         </article>
