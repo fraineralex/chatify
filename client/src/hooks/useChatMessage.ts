e8dc53dd3fa9df515454137ef9f8188957f33f6a
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-25 11:41:50 -0400

    Refactor useChatMessage hook to handle socket events and update chat state

--- client/src/hooks/useChatMessage.ts
+++ client/src/hooks/useChatMessage.ts
@@ -6,8 +6,9 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { io } from 'socket.io-client'
 import { Chat, Message, MessagesToRead, ServerMessageDB } from '../types/chat'
 import { useChatStore } from '../store/currenChat'
+import { getChatById } from '../services/chat'
 
-const SERVER_DOMAIN = import.meta.env.VITE_SERVER_DOMAIN ?? 'http://localhost:3000'
+const SERVER_DOMAIN = import.meta.env.VITE_SERVER_DOMAIN as string ?? 'http://localhost:3000'
 
 export const useChatMessage = () => {
   const currentChat = useChatStore(state => state.currentChat)
@@ -27,93 +28,101 @@ export const useChatMessage = () => {
 
 
   useEffect(() => {
-    const newSocket = io(SERVER_DOMAIN, {
-      auth: {
-        serverOffset: (socket?.auth as { serverOffset?: number })?.serverOffset ?? 0,
-        userId: loggedUser?.sub
-      }
-    })
+    ;(async () => {
 
-    setSocket(newSocket)
-
-    newSocket.on(SOCKET_EVENTS.CHAT_MESSAGE, (message: ServerMessageDB) => {
-      const newMessage: Message = {
-        uuid: message.uuid,
-        content: message.content,
-        createdAt: new Date(message.created_at),
-        senderId: message.sender_id,
-        receiverId: message.receiver_id,
-        chatId: message.chat_id,
-        type: message.type,
-        isDeleted: !!message.is_deleted,
-        isEdited: !!message.is_edited,
-        isRead: !!message.is_read,
-        replyToId: message.reply_to_id,
-        resourceUrl: message.resource_url
-      }
-
-      setMessage(newMessage)
-      setServerOffset(message.created_at)
-
-      const chat = chats.find(c => c.uuid === newMessage.chatId)
-      if (!chat) return
-
-      let unreadMessages = 0
-      if (loggedUser?.sub === newMessage?.receiverId) {
-        if (currentChat?.uuid === chat.uuid) {
-          const messagesToRead: MessagesToRead = {
-            chat_id: chat.uuid,
-            sender_id: newMessage.senderId,
-            receiver_id: newMessage.receiverId
-          }
-
-          newSocket.emit(SOCKET_EVENTS.READ_MESSAGE, messagesToRead)
-        } else {
-          unreadMessages = chat.unreadMessages + 1
+      const newSocket = io(SERVER_DOMAIN, {
+        auth: {
+          serverOffset: (socket?.auth as { serverOffset?: number })?.serverOffset ?? 0,
+          userId: loggedUser?.sub
         }
-      }
+      })
+    
+      setSocket(newSocket)
+      
+      newSocket.on(SOCKET_EVENTS.CHAT_MESSAGE, async (message: ServerMessageDB) => {
+        const newMessage: Message = {
+          uuid: message.uuid,
+          content: message.content,
+          createdAt: new Date(message.created_at),
+          senderId: message.sender_id,
+          receiverId: message.receiver_id,
+          chatId: message.chat_id,
+          type: message.type,
+          isDeleted: !!message.is_deleted,
+          isEdited: !!message.is_edited,
+          isRead: !!message.is_read,
+          replyToId: message.reply_to_id,
+          resourceUrl: message.resource_url
+        }
+        
+        setMessage(newMessage)
+        setServerOffset(message.created_at)
+        
+        let chat = chats.find(c => c.uuid === newMessage.chatId)
+        if (!chat) {
+          chat = await getChatById(newMessage.chatId, loggedUser?.sub)
+          chat.lastMessage = newMessage
+        }
 
-      const newChat: Chat = {
-        uuid: chat.uuid,
-        lastMessage: chat.lastMessage?.createdAt && new Date(chat.lastMessage.createdAt).getTime() > newMessage.createdAt.getTime() 
+        if (!chat) return
+
+        let unreadMessages = 0
+        if (loggedUser?.sub === newMessage?.receiverId) {
+          if (currentChat?.uuid === chat.uuid) {
+            const messagesToRead: MessagesToRead = {
+              chat_id: chat.uuid,
+              sender_id: newMessage.senderId,
+              receiver_id: newMessage.receiverId
+            }
+            
+            newSocket.emit(SOCKET_EVENTS.READ_MESSAGE, messagesToRead)
+          } else {
+            unreadMessages = chat.unreadMessages + 1
+          }
+        }
+        
+        const newChat: Chat = {
+          uuid: chat.uuid,
+          lastMessage: chat.lastMessage?.createdAt && new Date(chat.lastMessage.createdAt).getTime() > newMessage.createdAt.getTime() 
           ? chat.lastMessage 
           : newMessage,
-        user: chat.user,
-        createdAt: chat.createdAt,
-        unreadMessages
-      }
-
-      replaceChat(newChat)
-    })
-
-    newSocket.on(SOCKET_EVENTS.READ_MESSAGE, (message: ServerMessageDB) => {
-      const messageToReplace = messages.find(m => m.uuid === message.uuid)
-      if (!messageToReplace) return
-      messageToReplace.isRead = true
-      replaceMessage(messageToReplace)
-
-      const chat = chats.find(c => c.uuid === messageToReplace.chatId)
-      if (!chat || chat.lastMessage?.uuid !== messageToReplace.uuid) return
+          user: chat.user,
+          createdAt: chat.createdAt,
+          unreadMessages
+        }
+        
+        replaceChat(newChat)
+      })
       
-      const newChat: Chat = {
-        uuid: chat.uuid,
-        lastMessage: messageToReplace.createdAt.getTime() > (chat.lastMessage?.createdAt.getTime() ?? 0)
+      newSocket.on(SOCKET_EVENTS.READ_MESSAGE, (message: ServerMessageDB) => {
+        const messageToReplace = messages.find(m => m.uuid === message.uuid)
+        if (!messageToReplace) return
+        messageToReplace.isRead = true
+        replaceMessage(messageToReplace)
+        
+        const chat = chats.find(c => c.uuid === messageToReplace.chatId)
+        if (!chat || chat.lastMessage?.uuid !== messageToReplace.uuid) return
+        
+        const newChat: Chat = {
+          uuid: chat.uuid,
+          lastMessage: messageToReplace.createdAt.getTime() > (chat.lastMessage?.createdAt.getTime() ?? 0)
           ? messageToReplace 
           : chat.lastMessage,
-        user: chat.user,
-        createdAt: chat.createdAt,
-        unreadMessages: chat.unreadMessages - 1
+          user: chat.user,
+          createdAt: chat.createdAt,
+          unreadMessages: chat.unreadMessages - 1
+        }
+        
+        replaceChat(newChat)
+      })
+      
+      return () => {
+        newSocket.off(SOCKET_EVENTS.CHAT_MESSAGE)
+        newSocket.off(SOCKET_EVENTS.READ_MESSAGE)
       }
-
-      replaceChat(newChat)
-    })
-
-    return () => {
-      newSocket.off(SOCKET_EVENTS.CHAT_MESSAGE)
-      newSocket.off(SOCKET_EVENTS.READ_MESSAGE)
-    }
+    })()
   }, [loggedUser, currentChat])
-
+  
   useEffect(() => {
     ;(async () => {
       const response = await fetch(
