69513ca1dc985420080904c627fd9fa986d37743
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-25 10:41:59 -0400

    Refactor sorting logic in Sidebar component to use uniqueChats and improve performance

--- client/src/components/layout/sidebar.tsx
+++ client/src/components/layout/sidebar.tsx
@@ -1,16 +1,23 @@
 import { useSocketStore } from '../../store/socket'
+import { Chat, uuid } from '../../types/chat'
 import { ChatItem } from '../chat/chat-item'
 import { Header } from './header'
 
 export function Sidebar () {
   const { chats } = useSocketStore()
+  const uniqueChats = Object.values(
+    chats.reduce((acc: { [key: uuid]: Chat }, chat) => {
+      acc[chat.uuid] = chat
+      return acc
+    }, {})
+  )
 
   return (
     <div className='border-r h-screen'>
       <Header />
       <nav className='py-4 px-1 space-y-4 h-[90%]'>
         <ul className='space-y-1'>
-          {chats
+          {uniqueChats
             .sort(
               (a, b) =>
                 new Date(b.lastMessage?.createdAt || b.createdAt).getTime() -
