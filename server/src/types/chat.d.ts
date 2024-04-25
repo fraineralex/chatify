80bae7281924c2b002efa11eb4a3715d56f7731b
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-25 01:50:44 -0400

    Add Auth0User class to chat.d.ts

--- server/src/types/chat.d.ts
+++ server/src/types/chat.d.ts
@@ -67,3 +67,27 @@ export interface ChangeChat {
   uuid: uuid
   lastMessage?: Message
 }
+
+export declare class Auth0User {
+  name?: string;
+  given_name?: string;
+  family_name?: string;
+  middle_name?: string;
+  nickname?: string;
+  preferred_username?: string;
+  profile?: string;
+  picture?: string;
+  website?: string;
+  email?: string;
+  email_verified?: boolean;
+  gender?: string;
+  birthdate?: string;
+  zoneinfo?: string;
+  locale?: string;
+  phone_number?: string;
+  phone_number_verified?: boolean;
+  address?: string;
+  updated_at?: string;
+  sub?: string;
+  [key: string]: any;
+}
