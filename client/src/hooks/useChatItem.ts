17bfea4b2390946b423e6989b62e2a3f33eeb006
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-25 10:42:03 -0400

    Refactor useChatItem hook to include useNewChatModalStore and SERVER_DOMAIN constant

--- client/src/hooks/useChatItem.ts
+++ client/src/hooks/useChatItem.ts
@@ -2,8 +2,11 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import { useChatStore } from "../store/currenChat"
 import { useSocketStore } from "../store/socket"
-import { ChatItem, MessagesToRead, Chat, ChatDB } from "../types/chat"
+import { ChatItem, MessagesToRead, Chat } from "../types/chat"
 import { SOCKET_EVENTS } from "../constants"
+import { useNewChatModalStore } from "../store/newChatModal"
+
+const SERVER_DOMAIN = import.meta.env.VITE_SERVER_DOMAIN ?? 'http://localhost:3000'
 
 export function useChatItem ({ uuid, user, lastMessage, unreadMessages, createdAt, isNewChat }: ChatItem) {
     const { socket, setChat, chats } = useSocketStore()
@@ -12,9 +15,9 @@ export function useChatItem ({ uuid, user, lastMessage, unreadMessages, createdA
     const isCurrentChat = currentChat?.uuid === uuid
     const lastMessageDate = lastMessage?.createdAt || createdAt
     const chatExists = chats.some(chat => chat.user.id === user.id)
+    const closeModal = useNewChatModalStore(state => state.closeModal)
   
     const openChat = () => {
-        console.log('openChat')
       if (isCurrentChat || !socket || !uuid || !createdAt) return
   
       if (currentChat) localStorage.setItem(currentChat.uuid, currentChatDraft || '')
@@ -38,26 +41,27 @@ export function useChatItem ({ uuid, user, lastMessage, unreadMessages, createdA
       }
     }
 
-    const createChat = () => {
-        console.log('createChat')
+    const createChat = async () => {
         if (!isNewChat || chatExists) return
+        try {
 
-        const newChatDB: ChatDB = {
-            uuid: crypto.randomUUID(),
-            user1_id: loggedUser?.sub!,
-            user2_id: user.id,
-            created_at: new Date().toISOString()
+            const response = await fetch(`${SERVER_DOMAIN}/chats`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ user1_id: loggedUser?.sub!, user2_id: user.id })
+            })
+        
+            const newChat: Chat = await response.json()
+            newChat.user = user
+            
+            setChat(newChat)
+            setCurrentChat(newChat)
+            closeModal()
+        } catch (error) {
+            console.log(error)
         }
-
-        const newChat: Chat = {
-            uuid: newChatDB.uuid,
-            user,
-            createdAt: new Date(newChatDB.created_at),
-            unreadMessages: 0
-        }
-
-        setChat(newChat)
-        setCurrentChat(newChat)
     }
 
     return { openChat, isCurrentChat, lastMessageDate, loggedUser, chatExists, createChat}
