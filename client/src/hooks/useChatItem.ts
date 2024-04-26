5d94a08a8d4a0ba4f6959f1ee018afbf9ec6ad95
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-26 00:01:27 -0400

    Update useChatItem hook to handle new chat creation and open chat functionality

--- client/src/hooks/useChatItem.ts
+++ client/src/hooks/useChatItem.ts
@@ -2,38 +2,34 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import { useChatStore } from "../store/currenChat"
 import { useSocketStore } from "../store/socket"
-import { ChatItem, MessagesToRead, Chat } from "../types/chat"
+import { ChatItem, MessagesToRead } from "../types/chat"
 import { SOCKET_EVENTS } from "../constants"
 import { useNewChatModalStore } from "../store/newChatModal"
-
-const SERVER_DOMAIN = import.meta.env.VITE_SERVER_DOMAIN ?? 'http://localhost:3000'
+import { createChat } from "../services/chat"
 
 export function useChatItem ({ uuid, user, lastMessage, unreadMessages, createdAt, isNewChat }: ChatItem) {
     const { socket, setChat, chats } = useSocketStore()
     const { currentChat, setCurrentChat, currentChatDraft } = useChatStore()
     const { user: loggedUser } = useAuth0()
-    const isCurrentChat = currentChat?.uuid === uuid
+    const isCurrentChat = currentChat?.uuid === uuid || (isNewChat && currentChat?.user.id === user.id)
     const lastMessageDate = lastMessage?.createdAt || createdAt
     const chatExists = chats.some(chat => chat.user.id === user.id)
     const closeModal = useNewChatModalStore(state => state.closeModal)
   
-    const openChat = () => {
-      if (isCurrentChat || !socket || !uuid || !createdAt) return
-  
-      if (currentChat) localStorage.setItem(currentChat.uuid, currentChatDraft || '')
+    const handleOpenChat = () => {
+      console.log('openChat')
+      if (isCurrentChat || !socket) return
       
-      const newCurrentChat: Chat = {
-        uuid,
-        user,
-        lastMessage,
-        createdAt,
-        unreadMessages: unreadMessages ?? 0
-      }
+      if (currentChat) localStorage.setItem(currentChat.uuid, currentChatDraft || '')
+      const newCurrentChat = chats.find(chat => chat.user.id === user.id)
+      if (!newCurrentChat) return
       setCurrentChat(newCurrentChat)
+
+      if (isNewChat) closeModal()
   
       if (unreadMessages !== undefined && unreadMessages > 0) {
         const messagesToRead: MessagesToRead = {
-          chat_id: uuid,
+          chat_id: newCurrentChat.uuid,
           sender_id: user.id,
           receiver_id: loggedUser?.sub
         }
@@ -41,28 +37,16 @@ export function useChatItem ({ uuid, user, lastMessage, unreadMessages, createdA
       }
     }
 
-    const createChat = async () => {
-        if (!isNewChat || chatExists) return
-        try {
+    const handleCreateChat = async () => {
+        if (!isNewChat || chatExists ) return
+        const newChat = await createChat(loggedUser?.sub, user.id)
+        if (!newChat) return
 
-            const response = await fetch(`${SERVER_DOMAIN}/chats`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ user1_id: loggedUser?.sub!, user2_id: user.id })
-            })
-        
-            const newChat: Chat = await response.json()
-            newChat.user = user
-            
-            setChat(newChat)
-            setCurrentChat(newChat)
-            closeModal()
-        } catch (error) {
-            console.log(error)
-        }
+        newChat.user = user
+        setChat(newChat)
+        setCurrentChat(newChat)
+        closeModal()
     }
 
-    return { openChat, isCurrentChat, lastMessageDate, loggedUser, chatExists, createChat}
+    return { openChat: handleOpenChat, isCurrentChat, lastMessageDate, loggedUser, chatExists, createChat: handleCreateChat}
 }
