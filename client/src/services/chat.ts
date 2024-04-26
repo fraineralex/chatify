360f57d23997450ea6cca351e2f915202af9c512
Frainer Encarnacion <fraineralex2001@gmail.com>
2024-04-26 00:01:36 -0400

    Refactor getChatById function in client/src/services/chat.ts to handle errors and return undefined

--- client/src/services/chat.ts
+++ client/src/services/chat.ts
@@ -2,8 +2,41 @@ import { Chat } from "../types/chat"
 
 const SERVER_DOMAIN = import.meta.env.VITE_SERVER_DOMAIN as string ?? 'http://localhost:3000'
 
-export async function getChatById(chatId: string, userId: string | undefined): Promise<Chat> {
-  const response = await fetch(`${SERVER_DOMAIN}/chats/${chatId}?user_id=${userId}`)
+export async function getChatById(chatId: string, userId: string | undefined): Promise<Chat | undefined> {
+  try{
+    const response = await fetch(`${SERVER_DOMAIN}/chats/${chatId}?user_id=${userId}`)
+    return await response.json()
+  } catch (error) {
+    console.log(error)
+    return
+  }
+}
 
-  return await response.json()
+export async function createChat(user1Id: string | undefined, user2Id: string): Promise<Chat | undefined> {
+  try {
+    const response = await fetch(`${SERVER_DOMAIN}/chats`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ user1_id: user1Id, user2_id: user2Id })
+    })
+
+    return await response.json()
+  } catch (error) {
+      console.log(error)
+      return
+  }
+}
+
+export async function getAllChats(userId: string | undefined): Promise<Chat[] | undefined> {
+  try {
+    const response = await fetch(
+      `${SERVER_DOMAIN}/chats${userId ? `?user_id=${userId}` : ''}`
+    )
+    return await response.json()
+  } catch (error) {
+      console.log(error)
+      return
+  }
 }
