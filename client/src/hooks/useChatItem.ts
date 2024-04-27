5421c3ad4b9c2c57e7a5c2e97fe076143ee0cd0f
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-27 13:36:28 -0400

    Refactor useChatItem hook to improve readability and remove unused code

--- client/src/hooks/useChatItem.ts
+++ client/src/hooks/useChatItem.ts
@@ -1,52 +1,78 @@
 /* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
-import { useAuth0 } from "@auth0/auth0-react"
-import { useChatStore } from "../store/currenChat"
-import { useSocketStore } from "../store/socket"
-import { ChatItem, MessagesToRead } from "../types/chat"
-import { SOCKET_EVENTS } from "../constants"
-import { useNewChatModalStore } from "../store/newChatModal"
-import { createChat } from "../services/chat"
+import { useAuth0 } from '@auth0/auth0-react'
+import { useChatStore } from '../store/currenChat'
+import { useSocketStore } from '../store/socket'
+import { Chat, ChatItem, MessagesToRead } from '../types/chat'
+import { SOCKET_EVENTS } from '../constants'
+import { useNewChatModalStore } from '../store/newChatModal'
+import { createChat } from '../services/chat'
 
-export function useChatItem ({ uuid, user, lastMessage, unreadMessages, createdAt, isNewChat }: ChatItem) {
-    const { socket, setChat, chats } = useSocketStore()
-    const { currentChat, setCurrentChat, currentChatDraft } = useChatStore()
-    const { user: loggedUser } = useAuth0()
-    const isCurrentChat = currentChat?.uuid === uuid || (isNewChat && currentChat?.user.id === user.id)
-    const lastMessageDate = lastMessage?.createdAt || createdAt
-    const chatExists = chats.some(chat => chat.user.id === user.id)
-    const closeModal = useNewChatModalStore(state => state.closeModal)
-  
-    const handleOpenChat = () => {
-      console.log('openChat')
-      if (isCurrentChat || !socket) return
-      
-      if (currentChat) localStorage.setItem(currentChat.uuid, currentChatDraft || '')
-      const newCurrentChat = chats.find(chat => chat.user.id === user.id)
-      if (!newCurrentChat) return
-      setCurrentChat(newCurrentChat)
+export function useChatItem ({
+  uuid,
+  user,
+  unreadMessages,
+  isNewChat
+}: ChatItem) {
+  const { socket, addChat: setChat, chats, removeChat } = useSocketStore()
+  const { currentChat, setCurrentChat, currentChatDraft } = useChatStore()
+  const { user: loggedUser } = useAuth0()
+  const isCurrentChat =
+    currentChat?.uuid === uuid ||
+    (isNewChat && currentChat?.user.id === user.id)
+  const chatExists = chats.some(chat => chat.user.id === user.id)
+  const closeModal = useNewChatModalStore(state => state.closeModal)
 
-      if (isNewChat) closeModal()
-  
-      if (unreadMessages !== undefined && unreadMessages > 0) {
-        const messagesToRead: MessagesToRead = {
-          chat_id: newCurrentChat.uuid,
-          sender_id: user.id,
-          receiver_id: loggedUser?.sub
-        }
-        socket.emit(SOCKET_EVENTS.READ_MESSAGE, messagesToRead)
+  const handleOpenChat = () => {
+    if (isCurrentChat || !socket) return
+
+    if (currentChat)
+      localStorage.setItem(currentChat.uuid, currentChatDraft || '')
+    const newCurrentChat = chats.find(chat => chat.user.id === user.id)
+    if (!newCurrentChat) return
+    console.log('Antes de actualizar:', currentChat)
+    setCurrentChat(newCurrentChat)
+    console.log('Después de actualizar:', newCurrentChat)
+
+    if (isNewChat) closeModal()
+
+    if (unreadMessages !== undefined && unreadMessages > 0) {
+      const messagesToRead: MessagesToRead = {
+        chat_id: newCurrentChat.uuid,
+        sender_id: user.id,
+        receiver_id: loggedUser?.sub
       }
+
+      socket.emit(SOCKET_EVENTS.READ_MESSAGE, messagesToRead)
+    }
+  }
+
+  const handleCreateChat = async () => {
+    if (!isNewChat || chatExists) return
+    const newChat: Chat = {
+      uuid: crypto.randomUUID(),
+      user,
+      lastMessage: undefined,
+      createdAt: new Date(),
+      unreadMessages: 0
     }
 
-    const handleCreateChat = async () => {
-        if (!isNewChat || chatExists ) return
-        const newChat = await createChat(loggedUser?.sub, user.id)
-        if (!newChat) return
+    setChat(newChat)
+    setCurrentChat(newChat)
+    closeModal()
 
-        newChat.user = user
-        setChat(newChat)
-        setCurrentChat(newChat)
-        closeModal()
+    const newChatDB = await createChat(loggedUser?.sub, user.id, newChat.uuid)
+    if (!newChatDB) {
+      alert('Failed to create chat')
+      removeChat(newChat.uuid)
+      setCurrentChat(currentChat ?? null)
     }
+  }
 
-    return { openChat: handleOpenChat, isCurrentChat, lastMessageDate, loggedUser, chatExists, createChat: handleCreateChat}
+  return {
+    openChat: handleOpenChat,
+    isCurrentChat,
+    loggedUser,
+    chatExists,
+    createChat: handleCreateChat
+  }
 }
