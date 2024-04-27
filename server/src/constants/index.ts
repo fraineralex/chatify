d203c5aecad50a47805d79fab7c8f23a702b4072
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-27 13:38:08 -0400

    feat: Add RECOVER_MESSAGES socket event for message recovery functionality

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -7,6 +7,7 @@ export const SOCKET_EVENTS = {
   DELETE_MESSAGE: 'delete_message',
   READ_MESSAGE: 'read_message',
   CHANGE_CHAT: 'change_chat',
+  RECOVER_MESSAGES: 'recover_messages'
 } as const
 
 export const MESSAGES_TYPES = {
