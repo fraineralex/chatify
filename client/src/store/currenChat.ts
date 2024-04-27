adc9637a73b342b8dde551622099812289ad6118
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-27 13:37:18 -0400

    Refactor setCurrentChat function to handle null chat parameter and improve code readability

--- client/src/store/currenChat.ts
+++ client/src/store/currenChat.ts
@@ -2,22 +2,23 @@ import { create } from 'zustand'
 import { Chat } from '../types/chat'
 
 type ChatState = {
-    currentChat: Chat | null
-    currentChatDraft: string
-    setCurrentChat: (chat: Chat) => void
-    setCurrentChatDraft: (draft: string) => void
+  currentChat: Chat | null
+  currentChatDraft: string
+  setCurrentChat: (chat: Chat | null) => void
+  setCurrentChatDraft: (draft: string) => void
 }
 
-export const useChatStore = create<ChatState>((set) => ({
-    currentChat: null,
-    currentChatDraft: '',
-    
-    setCurrentChat: chat => {
-        const draft = localStorage.getItem(chat.uuid) || ''
+export const useChatStore = create<ChatState>(set => ({
+  currentChat: null,
+  currentChatDraft: '',
 
-        set({ currentChatDraft: draft})
-        set({ currentChat: chat })
-      },
-    
-      setCurrentChatDraft: draft => set({ currentChatDraft: draft }),
+  setCurrentChat: chat => {
+    if (!chat) return set({ currentChat: null })
+    const draft = localStorage.getItem(chat.uuid) || ''
+
+    set({ currentChatDraft: draft })
+    set({ currentChat: chat })
+  },
+
+  setCurrentChatDraft: draft => set({ currentChatDraft: draft })
 }))
