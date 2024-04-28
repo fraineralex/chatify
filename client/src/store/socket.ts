cf64c43ae2e4db7196a950631b4de899629174df
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-28 02:27:34 -0400

    Refactor socket.ts to sync the data with the IndexDB

--- client/src/store/socket.ts
+++ client/src/store/socket.ts
@@ -1,6 +1,7 @@
 import { create } from 'zustand'
 import { Chat, Chats, Message, Messages, uuid } from '../types/chat'
 import { Socket } from 'socket.io-client'
+import { db } from '../database/db'
 
 type SocketState = {
   socket: Socket | undefined
@@ -8,17 +9,20 @@ type SocketState = {
   setServerOffset: (serverOffset: Date) => void
   messages: Messages
   addMessage: (message: Message) => Promise<void>
-  replaceMessage: (message: Message) => void
+  replaceMessage: (message: Message) => Promise<void>
   chats: Chats
   addChat: (chat: Chat) => void
   replaceChat: (chat: Chat) => void
   removeChat: (chatUuid: uuid) => void
 }
 
+const initialMessages = (await db.messages.toArray()) ?? []
+const initialChats = (await db.chats.toArray()) ?? []
+
 export const useSocketStore = create<SocketState>((set, get) => ({
   socket: undefined,
-  messages: [],
-  chats: [],
+  messages: initialMessages,
+  chats: initialChats,
 
   setSocket: socket => set({ socket }),
   setServerOffset: (serverOffset: Date) => {
@@ -34,39 +38,47 @@ export const useSocketStore = create<SocketState>((set, get) => ({
 
   addMessage: async message => {
     const messages = get().messages
-    if (messages.some(m => m.uuid === message.uuid)) return
+    if (messages.some(m => m.uuid === message.uuid && !m.isSent)) {
+      get().replaceMessage(message)
+      return
+    }
     messages.push(message)
+    await db.messages.add(message)
     set({ messages })
   },
 
-  replaceMessage: message => {
+  replaceMessage: async message => {
     const messages = get().messages
     const index = messages.findIndex(m => m.uuid === message.uuid)
     if (index === -1) return
     messages[index] = message
+    await db.messages.put(message)
     set({ messages })
   },
 
-  addChat: chat => {
+  addChat: async chat => {
     const chats = get().chats
     if (chats.some(c => c.uuid === chat.uuid)) return
     chats.push(chat)
+    await db.chats.add(chat)
     set({ chats })
   },
 
-  replaceChat: chat => {
+  replaceChat: async chat => {
     const chats = get().chats
     const index = chats.findIndex(c => c.uuid === chat.uuid)
     if (index === -1) return
     chats[index] = chat
+    await db.chats.put(chat)
     set({ chats })
   },
 
-  removeChat: chatUuid => {
+  removeChat: async chatUuid => {
     const chats = get().chats
     const index = chats.findIndex(c => c.uuid === chatUuid)
     if (index === -1) return
     chats.splice(index, 1)
+    await db.chats.delete(chatUuid)
     set({ chats })
   }
 }))
