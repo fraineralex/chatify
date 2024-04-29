e009932faaa6474d2578bcc9f7e89057342bb738
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-29 01:45:29 -0400

    Refactor currentChat handling and remove unused code

--- client/src/store/currenChat.ts
+++ client/src/store/currenChat.ts
@@ -4,23 +4,14 @@ import { Chat } from '../types/chat'
 type ChatState = {
   currentChat: Chat | null
   getCurrentChat: () => Chat | null
-  currentChatDraft: string
   setCurrentChat: (chat: Chat | null) => void
-  setCurrentChatDraft: (draft: string) => void
 }
 
 export const useChatStore = create<ChatState>((set, get) => ({
   currentChat: null,
-  currentChatDraft: '',
 
   getCurrentChat: () => get().currentChat,
   setCurrentChat: chat => {
-    if (!chat) return set({ currentChat: null })
-    const draft = localStorage.getItem(chat.uuid) || ''
-
-    set({ currentChatDraft: draft })
-    set({ currentChat: chat })
-  },
-
-  setCurrentChatDraft: draft => set({ currentChatDraft: draft })
+    set({ currentChat: chat ?? null })
+  }
 }))
