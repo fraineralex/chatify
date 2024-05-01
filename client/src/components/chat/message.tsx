72149cf74c315d5492440b983d2cd2169161c0f0
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-01 13:29:13 -0400

    Refactor message component to include reply functionality and improve UI

--- client/src/components/chat/message.tsx
+++ client/src/components/chat/message.tsx
@@ -3,9 +3,11 @@ import { Message as MessageType, ReplyMessage, User } from '../../types/chat'
 import { useSocketStore } from '../../store/socket'
 import { MessageState } from './message-state'
 import { Pencil, Reply, SmilePlus, Trash2 } from 'lucide-react'
+import { QuotedMessage } from './quoted-message'
 
 interface Props extends MessageType {
   setReplyingMessage: (message: ReplyMessage) => void
+  messageListRef?: React.RefObject<HTMLUListElement>
 }
 
 export function Message ({
@@ -18,6 +20,8 @@ export function Message ({
   isRead,
   resourceUrl,
   type,
+  replyToId,
+  messageListRef,
   setReplyingMessage
 }: Props) {
   const { user: loggedUser } = useAuth0()
@@ -54,12 +58,13 @@ export function Message ({
   }
 
   return (
-    <article
+    <li
       className={`flex flex-col space-y-2 relative  ${
         isMe ? 'items-end' : 'items-start'
       }`}
+      data-uuid={uuid}
     >
-      <aside
+      <div
         className={`flex items-start gap-2 ${
           isMe ? 'flex-row-reverse' : 'flex-row'
         }`}
@@ -72,16 +77,23 @@ export function Message ({
           className='rounded-full align-top h-7 w-7 mt-2'
           style={{ aspectRatio: 50 / 50, objectFit: 'cover' }}
         />
-        <div
+        <aside
           className={`group flex items-center ${
             isMe ? 'place-content-start' : 'place-content-end'
           }`}
         >
-          <span
-            className={`items-center rounded-lg px-2 py-1 max-w-3xl whitespace-normal break-words ${
+          <article
+            className={`items-center rounded-lg pt-1 px-1 ps-2 max-w-3xl whitespace-normal break-words border border-transparent ${
               isMe ? 'bg-gray-300' : 'bg-gray-100'
             }`}
           >
+            {replyToId && (
+              <QuotedMessage
+                quotedMessageId={replyToId}
+                messageListRef={messageListRef}
+              />
+            )}
+
             <p className={`text-sm w-100 inline`}>{content}</p>
 
             {isMe && (
@@ -96,27 +108,27 @@ export function Message ({
             >
               {time}
             </time>
-          </span>
+          </article>
 
-          <span className='absolute -top-11 invisible group-hover:visible ease-in-out duration-100 bg-white border-1 rounded-md p-2 flex items-center space-x-2'>
-            <button className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-125 hover:text-blue-500'>
-              <SmilePlus />
+          <span className='absolute -top-11 invisible group-hover:visible ease-in-out duration-150 backdrop-blur-3xl border-1 rounded-md p-2 flex items-center space-x-2'>
+            <button className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-150 hover:text-blue-500'>
+              <SmilePlus className='w-4 h-4' />
             </button>
             <button
-              className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-125 hover:text-blue-500'
+              className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-150 hover:text-blue-500'
               onClick={handleReplyMessage}
             >
-              <Reply />
+              <Reply className='w-4 h-4' />
             </button>
-            <button className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-125 hover:text-blue-500'>
-              <Pencil />
+            <button className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-150 hover:text-blue-500'>
+              <Pencil className='w-4 h-4' />
             </button>
-            <button className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-125 hover:text-red-500'>
-              <Trash2 />
+            <button className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-150 hover:text-red-500'>
+              <Trash2 className='w-4 h-4' />
             </button>
           </span>
-        </div>
-      </aside>
-    </article>
+        </aside>
+      </div>
+    </li>
   )
 }
