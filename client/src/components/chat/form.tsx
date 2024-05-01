a1d68447e619a7e3bc8fd232985abb49b2767318
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-30 22:30:16 -0400

    Refactor chat form component to include replying functionality

--- client/src/components/chat/form.tsx
+++ client/src/components/chat/form.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import { AttachFile, Emoji, Send } from '../common/svg-icons'
-import { Message, ServerMessage } from '../../types/chat'
+import { Message, ReplyMessage, ServerMessage } from '../../types/chat'
 import { MESSAGES_TYPES, SOCKET_EVENTS } from '../../constants'
 import { useSocketStore } from '../../store/socket'
 import { useAuth0 } from '@auth0/auth0-react'
 import { useChatStore } from '../../store/currenChat'
+import { ReplyingMessage } from './replying-message'
 
-export function Form () {
+export function Form ({
+  replyingMessage,
+  setReplyingMessage
+}: {
+  replyingMessage: ReplyMessage | null
+  setReplyingMessage: (message: ReplyMessage | null) => void
+}) {
   const { socket, addMessage, replaceChat } = useSocketStore()
   const { currentChat, setCurrentChat } = useChatStore()
   const { user: loggedUser } = useAuth0()
@@ -29,7 +36,7 @@ export function Form () {
       is_edited: false,
       is_delivered: false,
       is_read: false,
-      reply_to_id: null,
+      reply_to_id: replyingMessage?.uuid || null,
       resource_url: null,
       created_at: new Date().toISOString()
     }
@@ -76,34 +83,37 @@ export function Form () {
   }
 
   return (
-    <form
-      className='flex items-center p-2 border-t w-full'
-      onSubmit={handleSubmit}
-    >
-      <button className='inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground p-4 rounded-full'>
-        <Emoji className='w-4 h-4' />
-        <span className='sr-only'>Insert emoji</span>
-      </button>
-      <button className='inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground p-4 rounded-full mx-2'>
-        <AttachFile className='w-4 h-4' />
-        <span className='sr-only'>Attach file</span>
-      </button>
-      <input
-        className='flex h-10 w-full border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-full border-0 flex-1'
-        placeholder='Type a message'
-        name='content'
-        autoFocus
-        onChange={handleChange}
-        value={currentChat?.draft ?? ''}
-        onBlur={handleBlur}
+    <form className='p-2 border-t w-full' onSubmit={handleSubmit}>
+      <ReplyingMessage
+        replyingMessage={replyingMessage}
+        setReplyingMessage={setReplyingMessage}
       />
-      <button
-        type='submit'
-        className='ms-2 inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground rounded-full w-6 h-6'
-      >
-        <Send className='w-5 h-5' />
-        <span className='sr-only'>Send</span>
-      </button>
+      <aside className='flex items-center'>
+        <button className='inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground p-4 rounded-full'>
+          <Emoji className='w-4 h-4' />
+          <span className='sr-only'>Insert emoji</span>
+        </button>
+        <button className='inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground p-4 rounded-full mx-2'>
+          <AttachFile className='w-4 h-4' />
+          <span className='sr-only'>Attach file</span>
+        </button>
+        <input
+          className='flex h-10 w-full border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 rounded-full border-0 flex-1'
+          placeholder='Type a message'
+          name='content'
+          autoFocus
+          onChange={handleChange}
+          value={currentChat?.draft ?? ''}
+          onBlur={handleBlur}
+        />
+        <button
+          type='submit'
+          className='ms-2 inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground rounded-full w-6 h-6'
+        >
+          <Send className='w-5 h-5' />
+          <span className='sr-only'>Send</span>
+        </button>
+      </aside>
     </form>
   )
 }
