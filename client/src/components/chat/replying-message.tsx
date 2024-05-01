25383bc3eb866b9fcf07ec63cfbdd92622b12ebe
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-01 13:29:41 -0400

    Refactor ReplyingMessage component to handle reply functionality and improve UI

--- client/src/components/chat/replying-message.tsx
+++ client/src/components/chat/replying-message.tsx
@@ -4,30 +4,32 @@ import { X } from 'lucide-react'
 
 export function ReplyingMessage ({
   replyingMessage,
-  setReplyingMessage
+  handleReplyMessage
 }: {
   replyingMessage: ReplyMessage | null
-  setReplyingMessage: (message: ReplyMessage | null) => void
+  handleReplyMessage: (message: ReplyMessage | null) => void
 }) {
   const { user: loggedUser } = useAuth0()
   if (!replyingMessage || !loggedUser) return null
 
   const accentColor =
     replyingMessage.user.id === loggedUser.sub
-      ? 'border-green-500 text-green-500'
-      : 'border-blue-500 text-blue-500'
+      ? 'border-blue-500 text-blue-500'
+      : 'border-green-500 text-green-500'
   return (
     <span
       className={`flex flex-col bg-gray-100 mx-5 mb-3 py-5 px-4 rounded-lg border-l-[5px] relative whitespace-normal break-words ${accentColor}`}
     >
       <button
         className='absolute top-0 right-0 m-2 text-gray-500 hover:scale-110 ease-in-out duration-100 hover:text-black'
-        onClick={() => setReplyingMessage(null)}
+        onClick={() => handleReplyMessage(null)}
       >
-        <X />
+        <X className='w-5 h-5' />
       </button>
-      <p className='font-medium'>{`Replying to ${replyingMessage.user.name}`}</p>
-      <p className='text-gray-600 text-md mt-1 line-clamp-2 max-w-full pr-10'>{replyingMessage.content}</p>
+      <p className='font-medium text-xs'>{`Replying to ${replyingMessage.user.name}`}</p>
+      <p className='text-gray-600 text-xs mt-1 line-clamp-2 max-w-full pr-10'>
+        {replyingMessage.content}
+      </p>
     </span>
   )
 }
