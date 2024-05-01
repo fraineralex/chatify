1302d0a22aa275b2850a6e1b9db24246a51ff59c
Frainer Encarnación <fraineralex2001@gmail.com>
2024-04-30 22:30:36 -0400

    Refactor message component to include reply functionality and improve UI

--- client/src/components/chat/message.tsx
+++ client/src/components/chat/message.tsx
@@ -1,31 +1,61 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import { Message as Props } from '../../types/chat'
+import { Message as MessageType, ReplyMessage, User } from '../../types/chat'
 import { useSocketStore } from '../../store/socket'
 import { MessageState } from './message-state'
+import { Pencil, Reply, SmilePlus, Trash2 } from 'lucide-react'
+
+interface Props extends MessageType {
+  setReplyingMessage: (message: ReplyMessage) => void
+}
 
 export function Message ({
+  uuid,
   content,
   createdAt,
   senderId,
   isSent,
   isDelivered,
-  isRead
+  isRead,
+  resourceUrl,
+  type,
+  setReplyingMessage
 }: Props) {
   const { user: loggedUser } = useAuth0()
+  if (!loggedUser) return null
   const { chats } = useSocketStore()
   const isMe = senderId === loggedUser?.sub
   const user = isMe
     ? loggedUser
     : chats.find(chat => chat.user.id === senderId)?.user
 
+  if (!user) return null
+
   const time = createdAt.toLocaleTimeString([], {
     hour: '2-digit',
     minute: '2-digit'
   })
 
+  const handleReplyMessage = () => {
+    let senderUser = user
+    if (isMe) {
+      senderUser = {
+        id: loggedUser?.sub,
+        name: 'yourself',
+        picture: loggedUser?.picture
+      }
+    }
+    setReplyingMessage({
+      uuid,
+      content,
+      resourceUrl,
+      type,
+      user: senderUser as User
+    })
+  }
+
   return (
     <article
-      className={`flex flex-col space-y-2 ${
+      className={`flex flex-col space-y-2 relative  ${
         isMe ? 'items-end' : 'items-start'
       }`}
     >
@@ -42,26 +72,50 @@ export function Message ({
           className='rounded-full align-top h-7 w-7 mt-2'
           style={{ aspectRatio: 50 / 50, objectFit: 'cover' }}
         />
-
-        <span
-          className={`flex items-center rounded-lg px-2 py-1 max-w-3xl ${
-            isMe ? 'bg-gray-300' : 'bg-gray-100'
+        <div
+          className={`group flex items-center ${
+            isMe ? 'place-content-start' : 'place-content-end'
           }`}
         >
-          <p className={`text-sm w-100 inline`}>{content}</p>
-          <time
-            className={`text-gray-500 float-end align-bottom text-[10px] mt-4 ms-5 inline`}
+          <span
+            className={`items-center rounded-lg px-2 py-1 max-w-3xl whitespace-normal break-words ${
+              isMe ? 'bg-gray-300' : 'bg-gray-100'
+            }`}
           >
-            {time}
-          </time>
-          {isMe && (
-            <MessageState
-              isSent={isSent}
-              isDelivered={isDelivered}
-              isRead={isRead}
-            />
-          )}
-        </span>
+            <p className={`text-sm w-100 inline`}>{content}</p>
+
+            {isMe && (
+              <MessageState
+                isSent={isSent}
+                isDelivered={isDelivered}
+                isRead={isRead}
+              />
+            )}
+            <time
+              className={`text-gray-500 float-end align-bottom text-[10px] mt-4 ms-5 inline`}
+            >
+              {time}
+            </time>
+          </span>
+
+          <span className='absolute -top-11 invisible group-hover:visible ease-in-out duration-100 bg-white border-1 rounded-md p-2 flex items-center space-x-2'>
+            <button className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-125 hover:text-blue-500'>
+              <SmilePlus />
+            </button>
+            <button
+              className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-125 hover:text-blue-500'
+              onClick={handleReplyMessage}
+            >
+              <Reply />
+            </button>
+            <button className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-125 hover:text-blue-500'>
+              <Pencil />
+            </button>
+            <button className='text-gray-800 cursor-pointer ease-linear duration-100 hover:scale-125 hover:text-red-500'>
+              <Trash2 />
+            </button>
+          </span>
+        </div>
       </aside>
     </article>
   )
