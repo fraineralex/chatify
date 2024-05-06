b8a8725b52529ef301ae1ac88d785fd86a3b4092
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-06 17:28:10 -0400

    Refactor GetUsersModal component to improve UI and add chat options

--- client/src/components/layout/get-users.tsx
+++ client/src/components/layout/get-users.tsx
@@ -1,10 +1,15 @@
-import { EllipsisVertical, SquarePen } from 'lucide-react'
+import {
+  Archive,
+  EllipsisVertical,
+  MegaphoneOff,
+  MessageSquareDot,
+  SquarePlus
+} from 'lucide-react'
 import Modal from '../common/modal'
 import { useEffect, useState } from 'react'
 import { User } from '../../types/chat'
 import { ChatItem } from '../chat/chat-item'
 import { useAuth0 } from '@auth0/auth0-react'
-import LogoutButton from '../auth/logout-button'
 import { useNewChatModalStore } from '../../store/newChatModal'
 import { getAllUsers } from '../../services/user'
 import { useSocketStore } from '../../store/socket'
@@ -25,19 +30,27 @@ export function GetUsersModal () {
 
   return (
     <>
-      <aside className='flex items-center justify-between px-3 py-2'>
+      <aside className='flex items-center justify-between px-3 py-2 flex-row-reverse text-gray-700 space-x-4'>
+        <button className='ps-2 hover:scale-110 hover:contrast-200'>
+          <EllipsisVertical className='w-5 h-5' />
+        </button>
         <button
-          className={`px-2 hover:scale-110 hover:contrast-200 ${
+          className={`hover:scale-110 hover:contrast-200 ${
             chats.length === 0
               ? 'text-blue-700 animate-pulse duration-300 ease-in-out'
               : 'text-gray-700'
           }`}
         >
-          <SquarePen className='w-5 h-5' onClick={openModal} />
+          <SquarePlus className='w-5 h-5' onClick={openModal} />
         </button>
-        <LogoutButton />
-        <button className='ps-2 text-gray-700 hover:scale-110 hover:contrast-200'>
-          <EllipsisVertical className='w-5 h-5' />
+        <button className='hover:scale-110 hover:contrast-200'>
+          <MessageSquareDot className='w-5 h-5' />
+        </button>
+        <button className='hover:scale-110 hover:contrast-200'>
+          <Archive className='w-5 h-5' />
+        </button>
+        <button className='hover:scale-110 hover:contrast-200'>
+          <MegaphoneOff className='w-5 h-5' />
         </button>
       </aside>
 
