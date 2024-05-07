5eaf99c1e9631fa6bcde474c1708df1c82297ae7
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-06 23:51:33 -0400

    Refactor Profile component to remove logout functionality and improve UI

--- client/src/components/auth/profile.tsx
+++ client/src/components/auth/profile.tsx
@@ -1,17 +1,14 @@
 import { useAuth0 } from '@auth0/auth0-react'
 
 export function Profile () {
-  const { user, logout } = useAuth0()
+  const { user } = useAuth0()
   const formatedName = user?.name?.split(' ').slice(0, 2).join(' ')
 
   return (
     <article
-      title='Log out'
-      aria-label='Log out'
-      className='flex justify-between items-center rounded-md group cursor-pointer'
-      onClick={() =>
-        logout({ logoutParams: { returnTo: window.location.origin } })
-      }
+      title={user?.name}
+      aria-label={user?.name}
+      className='flex justify-between items-center rounded-md group'
     >
       <figure className='flex justify-center items-center space-x-2 group-hover:space-x-3'>
         <img
@@ -19,12 +16,10 @@ export function Profile () {
           height='50'
           src={user?.picture}
           alt={`avatar of the user ${formatedName}`}
-          className='w-9 h-9 rounded-full group-hover:scale-105 ease-in-out duration-300'
+          className='w-10 h-10 rounded-full'
           style={{ aspectRatio: 50 / 50, objectFit: 'cover' }}
         />
-        <figcaption className='text-gray-700 font-semibold text-sm group-hover:text-gray-900 group-hover:scale-105 ease-in-out duration-300'>
-          {}
-        </figcaption>
+        <figcaption className='text-gray-700 font-semibold text-sm group-hover:text-gray-900 group-hover:scale-105 ease-in-out duration-300'></figcaption>
       </figure>
     </article>
   )
