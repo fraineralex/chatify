1be1adc46af28daed36d6ba4d8595580950d3859
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-06 23:52:21 -0400

    Refactor header component to use HeaderButtons instead of GetUsersModal

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -1,11 +1,11 @@
 import { Profile } from '../auth/profile'
-import { GetUsersModal } from './get-users'
+import { HeaderButtons } from './header-buttons'
 
 export function Header () {
   return (
-    <header className='flex items-center justify-between px-3 py-3 border-b '>
+    <header className='flex items-center justify-between px-3 py-3 border-b bg-gray-300 rounded-sm'>
       <Profile />
-      <GetUsersModal />
+      <HeaderButtons />
     </header>
   )
 }
