66c634573c7149bf3775f6f44fd61a62e5ba567b
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-09 11:56:39 -0400

    add(server): an patch endpoint to call the update curreent user metadata function

--- server/src/router/user.ts
+++ server/src/router/user.ts
@@ -4,3 +4,4 @@ import { UserController } from '../controllers/user.js'
 export const userRouter = Router()
 
 userRouter.get('/', UserController.getAll)
+userRouter.patch('/:userId', UserController.updateMetadata)
