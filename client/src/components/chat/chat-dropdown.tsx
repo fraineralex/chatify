020031c17a4e3a633668d620cafac9aaa378d71b
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-09 11:45:33 -0400

    add(client): handleClickPinChat function using the current user metadata

--- client/src/components/chat/chat-dropdown.tsx
+++ client/src/components/chat/chat-dropdown.tsx
@@ -11,15 +11,36 @@ import { Dropdown } from '../common/dropdown'
 import { uuid } from '../../types/chat'
 import { useSocketStore } from '../../store/socket'
 import { useChatStore } from '../../store/currenChat'
+import { useUserMetadata } from '../../hooks/useUserMetadata'
 
 export function ChatDropdown ({ uuid }: { uuid: uuid }) {
-  const { removeChat } = useSocketStore()
+  const { removeChat, chats } = useSocketStore()
   const { currentChat, setCurrentChat } = useChatStore()
+  const { userMetadata, updateUserMetadata } = useUserMetadata()
 
-  const handleDeleteChat = () => {
+  const handleDeleteChat = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
     removeChat(uuid)
     if (currentChat?.uuid === uuid) setCurrentChat(null)
   }
+
+  const handleClickPinChat = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
+    const chat = chats.find(chat => chat.uuid === uuid)
+    if (!chat) return
+    await updateUserMetadata({
+      chat_preferences: {
+        ...userMetadata.chat_preferences,
+        pinned: [...userMetadata.chat_preferences.pinned, chat.uuid]
+      }
+    })
+  }
+
+  
 
   return (
     <Dropdown Icon={<ChevronDown className='w-5 h-5' />}>
@@ -46,7 +67,10 @@ export function ChatDropdown ({ uuid }: { uuid: uuid }) {
           </button>
         </li>
         <li>
-          <button className='flex px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white w-full text-left align-middle'>
+          <button
+            className='flex px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white w-full text-left align-middle'
+            onClick={handleClickPinChat}
+          >
             <Pin className='w-5 h-5 inline me-2' /> Pin chat
           </button>
         </li>
