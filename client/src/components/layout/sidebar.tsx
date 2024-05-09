4e36e9ed7115efed92ab42f5c22d05367b054c63
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-09 11:48:36 -0400

    fix(client): update the chats sort to show the pinned chats first

--- client/src/components/layout/sidebar.tsx
+++ client/src/components/layout/sidebar.tsx
@@ -12,11 +12,13 @@ export function Sidebar () {
       <Header />
       <nav className='py-4 px-1 space-y-4 h-[90%]'>
         <ul className='space-y-1'>
-          {chats
+          {[...chats]
             .sort(
               (a, b) =>
                 new Date(b.lastMessage?.createdAt || b.createdAt).getTime() -
-                new Date(a.lastMessage?.createdAt || a.createdAt).getTime()
+                (b.isPinned ? Number.MAX_SAFE_INTEGER : 0) -
+                new Date(a.lastMessage?.createdAt || a.createdAt).getTime() -
+                (a.isPinned ? Number.MAX_SAFE_INTEGER : 0)
             )
             .map((chat, index) => (
               <ChatItem key={index} {...chat} />
