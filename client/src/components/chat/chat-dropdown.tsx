87e74bde298281b9edbfaa4a024f6ff1d74f4ade
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-11 17:37:11 -0400

    feat(client): add the toggle read/unread option to the chat dropdown menu

--- client/src/components/chat/chat-dropdown.tsx
+++ client/src/components/chat/chat-dropdown.tsx
@@ -13,16 +13,17 @@ import {
   Trash2
 } from 'lucide-react'
 import { Dropdown } from '../common/dropdown'
-import { uuid } from '../../types/chat'
+import { MessagesToUpdate, uuid } from '../../types/chat'
 import { useSocketStore } from '../../store/socket'
 import { useChatStore } from '../../store/currenChat'
 import { useUserMetadata } from '../../hooks/useUserMetadata'
 import React from 'react'
 import { toggleChatBlock } from '../../services/chat'
 import { useAuth0 } from '@auth0/auth0-react'
+import { SOCKET_EVENTS } from '../../constants'
 
 export function ChatDropdown ({ uuid }: { uuid: uuid }) {
-  const { chats, replaceChat } = useSocketStore()
+  const { chats, replaceChat, socket } = useSocketStore()
   const { currentChat, setCurrentChat } = useChatStore()
   const { userMetadata, updateUserMetadata } = useUserMetadata()
   const { user } = useAuth0()
@@ -186,6 +187,27 @@ export function ChatDropdown ({ uuid }: { uuid: uuid }) {
     }
   }
 
+  const handleToggleReadChat = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
+    if (!user) return
+
+    if (chat.unreadMessages > 0) {
+      const messagesToUpdate: MessagesToUpdate = {
+        chat_id: chat.uuid,
+        sender_id: user.sub!,
+        receiver_id: chat.user.id
+      }
+      return socket?.emit(SOCKET_EVENTS.READ_MESSAGE, messagesToUpdate)
+    }
+
+    replaceChat({
+      ...chat,
+      isUnread: !chat.isUnread
+    })
+  }
+
   return (
     <Dropdown Icon={<ChevronDown className='w-5 h-5' />}>
       <ul
@@ -254,16 +276,19 @@ export function ChatDropdown ({ uuid }: { uuid: uuid }) {
           </button>
         </li>
         <li>
-          <button className='flex px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white w-full text-left align-middle'>
-            {chat.isDeleted ? (
-              <>
-                <MessageSquareDot className='w-5 h-5 inline me-2' />
-                Mark as unread
-              </>
-            ) : (
+          <button
+            className='flex px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white w-full text-left align-middle'
+            onClick={handleToggleReadChat}
+          >
+            {chat.unreadMessages > 0 || chat.isUnread ? (
               <>
                 <MessageSquare className='w-5 h-5 inline me-2' />
                 Mark as read
+              </>
+            ) : (
+              <>
+                <MessageSquareDot className='w-5 h-5 inline me-2' />
+                Mark as unread
               </>
             )}
           </button>
