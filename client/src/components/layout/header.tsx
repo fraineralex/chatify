691156a7ecbbc6d70e3cf4e6dce33709cdc41668
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-13 12:30:15 -0400

    refactor(client): update Header component background color in HeaderButtons

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -3,7 +3,7 @@ import { HeaderButtons } from './header-buttons'
 
 export function Header () {
   return (
-    <header className='flex items-center justify-between px-3 py-3 border-b bg-gray-300 rounded-sm'>
+    <header className='flex items-center justify-between px-3 py-3 border-b bg-gray-200 rounded-sm'>
       <Profile />
       <HeaderButtons />
     </header>
