01f0fda926a828eeab619c6c8178d9ccf8150215
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-13 12:30:51 -0400

    refactor(client): update grid layout in ChatScreen component to span 7 columns

--- client/src/pages/chat-screen.tsx
+++ client/src/pages/chat-screen.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from '../components/layout/sidebar'
 
 export function ChatScreen () {
   return (
-    <section className='grid grid-cols-4 h-screen border rounded-lg overflow-hidden font-inter'>
+    <section className='grid grid-cols-7 h-screen border rounded-lg overflow-hidden font-inter'>
       <Sidebar />
       <Chat />
     </section>
