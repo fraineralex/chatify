ceb1fcd35889967786681b2fb7ea8c7875a3e517
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-15 13:07:00 -0400

    refactor(client): update user service to include updateUserMetadata function

--- client/src/services/user.ts
+++ client/src/services/user.ts
@@ -1,14 +1,30 @@
-import { User } from "../types/chat"
+import { User } from '../types/chat'
+import { metadata } from '../types/user'
 
 const SERVER_DOMAIN =
   import.meta.env.VITE_SERVER_DOMAIN ?? 'http://localhost:3000'
-  
-export async function getAllUsers(): Promise<User[] | undefined> {
-    try {
-      const response = await fetch(`${SERVER_DOMAIN}/users`)
-      return await response.json()
-    } catch (error) {
-        console.log(error)
-        return
-    }
+
+export async function getAllUsers (): Promise<User[] | undefined> {
+  try {
+    const response = await fetch(`${SERVER_DOMAIN}/users`)
+    return await response.json()
+  } catch (error) {
+    console.log(error)
+    return
   }
+}
+
+export const updateUserMetadata = async (
+  metadata: metadata,
+  userSub: string | undefined
+) => {
+  const data = await fetch(`${SERVER_DOMAIN}/users/${userSub}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ metadata })
+  })
+
+  return data
+}
