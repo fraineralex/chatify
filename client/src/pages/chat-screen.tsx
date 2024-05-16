76dc782e215820af3fb4ea14ec36da323b3a6aa7
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-15 23:15:17 -0400

    refactor(client): Remove useUserMetadata hook from ChatScreen component

--- client/src/pages/chat-screen.tsx
+++ client/src/pages/chat-screen.tsx
@@ -1,7 +1,9 @@
 import { Chat } from '../components/chat'
 import { Sidebar } from '../components/layout/sidebar'
+import { useUserMetadata } from '../hooks/useUserMetadata'
 
 export function ChatScreen () {
+  useUserMetadata()
   return (
     <section className='grid grid-cols-7 h-screen border rounded-lg overflow-hidden font-inter'>
       <Sidebar />
