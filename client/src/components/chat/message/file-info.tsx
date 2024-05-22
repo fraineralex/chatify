c9e088e2924dbc51afbc116009fb2476be99c4b8
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-22 17:55:36 -0400

    refactor(client): Improve file information display in FileInfo component

--- client/src/components/chat/message/file-info.tsx
+++ client/src/components/chat/message/file-info.tsx
@@ -1,43 +1,24 @@
-import { FILE_ICONS, MIME_TYPE_MAP } from '../../../constants'
+import { FILE_ICONS } from '../../../constants'
 import { StaticFile } from '../../../types/chat'
+import { FileMetadata } from './file-metadata'
 
 export function FileInfo ({ file }: { file: StaticFile }) {
-  console.log(file)
   const fileExtension =
     file.filename?.split('.').pop()?.toLowerCase() ?? 'unknown'
-  const Icon = FILE_ICONS[fileExtension] || FILE_ICONS.unknown
-  const fileType = file.contentType
-    ? MIME_TYPE_MAP[file.contentType]
-    : `${fileExtension.toUpperCase()} File`
-
-  let fileWeight = ''
-  if (file.contentLength) {
-    const bytes = file.contentLength
-    const kilobytes = bytes / 1024
-    const megabytes = kilobytes / 1024
-
-    switch (true) {
-      case bytes < 1024:
-        fileWeight = `${bytes.toFixed(2)} B,`
-        break
-      case kilobytes < 1024:
-        fileWeight = `${kilobytes.toFixed(2)} KB,`
-        break
-      default:
-        fileWeight = `${megabytes.toFixed(2)} MB,`
-    }
-  }
+  const Icon = FILE_ICONS[fileExtension] ?? FILE_ICONS.unknown
   return (
     <figure className='rounded-md bg-gray-200 mx-1 mt-1'>
       <div className='flex space-x-2 border-b p-2 border-gray-400'>
         <Icon className='w-12 h-12' />
-        <span>
-          <h6 className='font-medium text-base w-64 truncate block'>
+        <span className='text-gray-500'>
+          <h6 className='font-medium text-base w-64 truncate block text-gray-800'>
             {file.filename}
           </h6>
-          <small className='block not-italic text-gray-500 font-medium'>
-            {fileWeight} {fileType}
-          </small>
+          <FileMetadata
+            contentLength={file.contentLength}
+            contentType={file.contentType}
+            fileExtension={fileExtension}
+          />
         </span>
       </div>
       <span className='flex place-content-center justify-around p-2 space-x-3 mt-1'>
