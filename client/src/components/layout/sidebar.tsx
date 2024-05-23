3caf0ba090883ee0f78662484965570c94bdfae4
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-23 18:44:39 -0400

    feat(client): add support to filtering chats in the sidebar

--- client/src/components/layout/sidebar.tsx
+++ client/src/components/layout/sidebar.tsx
@@ -3,10 +3,12 @@ import { useSocketStore } from '../../store/socket'
 import { ChatItem } from '../chat/chat-item'
 import { Header } from './header'
 import { Chat } from '../../types/chat'
+import { useState } from 'react'
 
 export function Sidebar () {
   const { areChatsLoaded } = useChatMessage()
   const { chatFilterState, setChatFilterState, chats } = useSocketStore()
+  const [search, setSearch] = useState('')
 
   const filters: {
     [key: string]: (chat: Chat) => boolean | string | undefined | null
@@ -19,12 +21,32 @@ export function Sidebar () {
       !chat.isDeleted && (chat.unreadMessages > 0 || chat.isUnread)
   }
 
-  let notChatsText =
+  const notChatsText =
     chatFilterState === 'all'
       ? "🚨 You don't have any chat yet"
       : `🚨 You don't have ${chatFilterState} chat yet`
 
-  if (chatFilterState === 'search') notChatsText = 'No chat found'
+  const filteredChats = chats
+    .filter(chat => chat.user.name.toLowerCase().includes(search.toLowerCase()))
+    .filter(filters[chatFilterState])
+    .sort((a, b) => {
+      const dateA = a.lastMessage
+        ? new Date(a.lastMessage.createdAt)
+        : new Date(a.createdAt)
+      const dateB = b.lastMessage
+        ? new Date(b.lastMessage.createdAt)
+        : new Date(b.createdAt)
+
+      if (a.isPinned !== b.isPinned) {
+        return b.isPinned ? 1 : -1
+      }
+
+      if (a.isPinned && b.isPinned) {
+        return dateB.getTime() - dateA.getTime()
+      }
+
+      return dateB.getTime() - dateA.getTime()
+    })
 
   return (
     <div className='border-r h-screen col-span-2'>
@@ -35,8 +57,10 @@ export function Sidebar () {
           type='text'
           className='w-full placeholder-gray-700 bg-gray-200 outline-none text-sm text-gray-800 focus:border-gray-300 rounded-lg px-2 py-2'
           placeholder='Search'
+          onChange={({ target }) => setSearch(target.value)}
+          value={search}
         />
-        {chatFilterState !== 'all' && chatFilterState !== 'search' && (
+        {chatFilterState !== 'all' && (
           <span className='flex place-content-center space-x-4 mt-2 text-gray-800'>
             <button
               className='bg-gray-200 py-2 px-3 rounded-full text-xs hover:bg-gray-300'
@@ -52,29 +76,13 @@ export function Sidebar () {
       </div>
       <nav className='pb-2 px-1 space-y-4 h-[90%]'>
         <ul>
-          {chats
-            .filter(filters[chatFilterState])
-            .sort((a, b) => {
-              const dateA = a.lastMessage
-                ? new Date(a.lastMessage.createdAt)
-                : new Date(a.createdAt)
-              const dateB = b.lastMessage
-                ? new Date(b.lastMessage.createdAt)
-                : new Date(b.createdAt)
-
-              if (a.isPinned !== b.isPinned) {
-                return b.isPinned ? 1 : -1
-              }
-
-              if (a.isPinned && b.isPinned) {
-                return dateB.getTime() - dateA.getTime()
-              }
-
-              return dateB.getTime() - dateA.getTime()
-            })
-            .map((chat, index) => (
-              <ChatItem key={index} {...chat} />
-            ))}
+          {filteredChats.length > 0
+            ? filteredChats.map((chat, index) => (
+                <ChatItem key={index} {...chat} />
+              ))
+            : search && (
+                <p className='text-center font-medium'>Chat not found</p>
+              )}
           {chats.length === 0 && (
             <p className='text-center font-medium'>
               {areChatsLoaded ? notChatsText : 'Loading chats...'}
