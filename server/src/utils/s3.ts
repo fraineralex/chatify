3b62a37edc49b034ff90a7ca2ee28aa8acec7bfa
Frainer Encarnación <fraineralex2001@gmail.com>
2024-05-25 12:21:59 -0400

    refactor: Update getObjectSignedUrl function to include CloudFront CDN signing

--- server/src/utils/s3.ts
+++ server/src/utils/s3.ts
@@ -1,17 +1,20 @@
 import {
   S3Client,
   PutObjectCommand,
-  GetObjectCommand,
   DeleteObjectCommand,
   HeadObjectCommand
 } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { CloudfrontSignInput, getSignedUrl } from '@aws-sdk/cloudfront-signer'
 import { ResourceData } from '../types/chat.js'
 
 const s3BucketName = process.env.S3_BUCKET_NAME ?? ''
 const s3BucketRegion = process.env.S3_BUCKET_REGION ?? ''
 const s3AccessKeyId = process.env.S3_ACCESS_KEY_ID ?? ''
 const s3SecretAccessKey = process.env.S3_SECRET_ACCESS_KEY ?? ''
+
+const cloudfrontUrl = process.env.CLOUDFRONT_URL ?? ''
+const cloudfrontPrivateKey = process.env.CLOUDFRONT_PRIVATE_KEY ?? ''
+const cloudfrontKeyPairId = process.env.CLOUDFRONT_KEY_PAIR_ID ?? ''
 
 const s3 = new S3Client({
   region: s3BucketRegion,
@@ -43,24 +46,41 @@ export async function getObjectSignedUrl (key: string) {
     Bucket: s3BucketName,
     Key: key
   }
-  const getObjectCommand = new GetObjectCommand(objectParams)
+
+  const expirationTime = 60 * 60 * 24 // 24 hours
+
+  const cloudfrontSignParams: CloudfrontSignInput = {
+    url: `${cloudfrontUrl}/${key}`,
+    privateKey: cloudfrontPrivateKey,
+    keyPairId: cloudfrontKeyPairId,
+    policy: JSON.stringify({
+      Statement: [
+        {
+          Resource: `${cloudfrontUrl}/${key}`,
+          Condition: {
+            DateLessThan: {
+              'AWS:EpochTime': Math.floor(Date.now() / 1000 + expirationTime)
+            }
+          }
+        }
+      ]
+    })
+  }
+
   let contentType = ''
   let contentLength = 0
   let filename = key
   try {
     const isDocument = key.split('.').length > 2
     if (isDocument) {
-      const headObjectCommand = new HeadObjectCommand(objectParams)
-      const metadata = await s3.send(headObjectCommand)
+      const metadata = await s3.send(new HeadObjectCommand(objectParams))
 
       contentType = metadata.ContentType ?? ''
       contentLength = metadata.ContentLength ?? 0
       filename = `${key.split('.')[0]}.${key.split('.').pop()}`
     }
-    const expirationTime = 3600 // seconds
-    const url = await getSignedUrl(s3, getObjectCommand, {
-      expiresIn: expirationTime
-    })
+    const url = getSignedUrl(cloudfrontSignParams)
+    console.log(url)
 
     return {
       url,
