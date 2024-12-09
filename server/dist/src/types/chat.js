b78ae5902f33b1f45c99fb73c2715e6771650af4
Frainer Encarnación <fraineralex2001@gmail.com>
2024-12-09 15:23:10 -0400

    fix(server): remove server/dist directory

--- server/dist/src/types/chat.js
+++
@@ -1,5 +0,0 @@
-
-!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="62622897-5a65-57e6-aca4-f51f534bae6c")}catch(e){}}();
-export {};
-//# sourceMappingURL=chat.js.map
-//# debugId=62622897-5a65-57e6-aca4-f51f534bae6c
--- server/dist/src/types/socket.js
+++
@@ -1,5 +0,0 @@
-
-!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="c29ae288-55da-5678-813f-087d8331c37e")}catch(e){}}();
-export {};
-//# sourceMappingURL=socket.js.map
-//# debugId=c29ae288-55da-5678-813f-087d8331c37e
